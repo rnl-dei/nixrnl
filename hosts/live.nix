e6938bc3ea0056ab02c3fee2d2821aaa6a4ad49d
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2023-07-20 14:54:28 +0100

    hosts/live: disable node exporter

--- hosts/live.nix
+++ hosts/live.nix
@@ -7,4 +7,7 @@
     (inputs.nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
     profiles.core.rnl
   ];
+
+  # Disable Node exporter
+  services.prometheus.exporters.node.enable = false;
 }
