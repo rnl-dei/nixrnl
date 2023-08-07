703261af6f1acadef72945ba7c94d021be024e52
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2023-08-07 22:37:33 +0100

    hosts/live: autologin as root

--- hosts/live.nix
+++ hosts/live.nix
@@ -17,11 +17,11 @@
     location = null;
   };
 
-  # Set simple root password
-  users.users.root.initialPassword = "nixos@rnl";
-
   # Disable root SSH via password
   services.openssh.settings.PermitRootLogin = lib.mkForce "without-password";
+
+  # Disable nixos autologin and set root as autologin user
+  services.getty.autologinUser = lib.mkForce "root";
 
   # Disable Node exporter
   services.prometheus.exporters.node.enable = false;
