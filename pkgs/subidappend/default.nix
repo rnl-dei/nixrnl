b0e86b75f2d8cfec855c726ba24b53116ea75e5b
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2023-08-22 15:34:50 +0100

    pkgs/subidappend: add package
    Co-authored-by: Carlos Vaz <carlos.vaz@rnl.tecnico.ulisboa.pt>

---
+++ pkgs/subidappend/default.nix
@@ -0,0 +1,26 @@
+{
+  lib,
+  stdenv,
+  ...
+}:
+stdenv.mkDerivation rec {
+  pname = "subidappend";
+  version = "1.0";
+
+  src = lib.cleanSource ./.;
+
+  buildInputs = [];
+
+  buildPhase = ''
+    gcc subidappend.c -o subidappend
+  '';
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp subidappend $out/bin
+  '';
+
+  meta = with lib; {
+    maintainers = ["carlos.vaz"];
+  };
+}
