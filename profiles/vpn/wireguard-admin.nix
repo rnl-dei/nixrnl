f988547b6f3267c68625902c88085fdf431ab221
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2023-08-22 11:38:17 +0100

    profiles/vpn/wireguard-admin: add profile

---
+++ profiles/vpn/wireguard-admin.nix
@@ -0,0 +1,75 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  listenPort = 34266;
+
+  outInterface = config.networking.nat.externalInterface;
+  hosts = config.rnl.vpn.wireguard-admin.hosts;
+
+  # Allowed IPs will look like this:
+  # 192.168.20.<lastOctect> fd92:3315:9e43:c490::<lastOctect>/128 (plus extra ips for multicast)
+  mkPeers = builtins.map (peer: {
+    inherit (peer) publicKey;
+    allowedIPs = ["192.168.20.${toString peer.lastOctect}/32" "fd92:3315:9e43:c490::${toString peer.lastOctect}/128" "224.0.0.0/24" "ff00::/16"];
+  });
+in {
+  networking.nat.enable = true;
+  assertions = [
+    {
+      condition = "config.networking.nat.externalInterface == null";
+      message = "config.networking.nat.externalInterface must be set";
+    }
+  ];
+  networking.nat.internalInterfaces = ["wg0"];
+  networking.firewall = {
+    allowedUDPPorts = [listenPort];
+  };
+
+  age.secrets."wireguard-admin-private.key" = {
+    file = ../../secrets/wireguard-admin-private-key.age;
+    mode = "0400";
+    owner = "root";
+  };
+
+  networking.wireguard.interfaces = {
+    wg0 = {
+      inherit listenPort;
+      privateKeyFile = config.age.secrets."wireguard-admin-private.key".path;
+      ips = ["192.168.20.254/24"];
+      postSetup = ''
+        ${pkgs.iptables}/bin/iptables -t nat -A POSTROUTING -s 192.168.20.0/24 -o ${outInterface} -j MASQUERADE
+      '';
+
+      # This undoes the above command
+      postShutdown = ''
+        ${pkgs.iptables}/bin/iptables -t nat -D POSTROUTING -s 192.168.20.0/24 -o ${outInterface} -j MASQUERADE
+      '';
+
+      peers = mkPeers hosts;
+    };
+  };
+
+  services.keepalived = {
+    enable = lib.mkDefault true;
+    vrrpInstances.wireguardAdminIP4 = {
+      virtualRouterId = 211;
+      interface = outInterface;
+      virtualIps = [{addr = "193.136.164.211/27";}]; # fang IPv4
+    };
+    vrrpInstances.wireguardAdminIP6 = {
+      virtualRouterId = 211;
+      interface = outInterface;
+      virtualIps = [{addr = "2001:690:2100:82::211/64";}]; # fang IPv6
+    };
+  };
+
+  # Configure wireguard exporter
+  services.prometheus.exporters.wireguard = {
+    enable = lib.mkDefault true;
+    openFirewall = true;
+    withRemoteIp = lib.mkForce false; # Don't track remote IPs
+  };
+}
---
+++ secrets/secrets.nix
@@ -0,0 +1,15 @@
+let
+  # Public SSH keys of users
+  torvalds = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIP/rKlyYzFscsso96forbN2Y6IJ5yitGPS9Nci5n9vps";
+  users = [torvalds];
+
+  deployMachines = users ++ [];
+
+  # Public SSH host keys of hosts
+  hagrid = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN9MnzWv7ulk6w3YTEIW5XuW6CzpMd43qFYpfsQ3zt7k";
+in {
+  # Host keys only need to be accessible by the deploy machines
+
+  # Secrets
+  "wireguard-admin-private-key.age".publicKeys = users ++ [hagrid];
+}
