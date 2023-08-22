4d0e018f28ab8391777c080dd6aef3103a75da35
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2023-08-22 19:35:16 +0100

    hosts/live: add dhcp

--- hosts/live/configuration.nix
+++ hosts/live/configuration.nix
@@ -1,6 +1,21 @@
-{inputs, ...}: {
+{
+  inputs,
+  lib,
+  ...
+}: {
   aliases = {
-    live = { extraModules = [(inputs.nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")]; };
-    live-netboot = { extraModules = [(inputs.nixpkgs + "/nixos/modules/installer/netboot/netboot-minimal.nix")]; };
+    live = {extraModules = [(inputs.nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")];};
+    live-dhcp = {
+      extraModules = [
+        (inputs.nixpkgs + "/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix")
+        {networking.useDHCP = lib.mkForce true;}
+      ];
+    };
+    live-netboot-dhcp = {
+      extraModules = [
+        (inputs.nixpkgs + "/nixos/modules/installer/netboot/netboot-minimal.nix")
+        {networking.useDHCP = lib.mkForce true;}
+      ];
+    };
   };
 }
