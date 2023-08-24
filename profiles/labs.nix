3325c93a4f79b7db358e27a385dcc446646f6215
Carlos Vaz <carlos.vaz@rnl.tecnico.ulisboa.pt>
2023-08-24 17:59:55 +0100

    profiles/labs: set root password

--- profiles/labs.nix
+++ profiles/labs.nix
@@ -30,6 +30,8 @@
 
   # Clean subuids and gids on boot
   systemd.tmpfiles.rules = ["f+  /etc/subuid 0644 root root -" "f+  /etc/subgid 0644 root root -"];
+  users.users.root.hashedPassword =
+    "$y$j9T$kLiDSrbLRV1LUo5yxocDv.$v5cptSarCIF4y.h6R5JTl8TLgfncHE8ZXKignjsF2i2";
 
   # Disable Network Manager
   networking.networkmanager.enable = false;
