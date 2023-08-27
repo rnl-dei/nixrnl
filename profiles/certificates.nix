e2aad48bb683d92f2ec2f790a367f59d4ea35dcf
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2023-08-27 21:50:05 +0100

    profiles/certificates: add profile

---
+++ profiles/certificates.nix
@@ -0,0 +1,32 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  defaultACMEServer =
+    lib.mkIf
+    config.rnl.internalHost
+    "${config.rnl.vault.url}/v1/pki/acme/directory";
+in {
+  # DHParams (Enable if you want to use DHE)
+  security.dhparams = {
+    defaultBitSize = 2048; # Recommended value
+  };
+
+  # ACME
+  security.acme = {
+    acceptTerms = true;
+    defaults = {
+      email = "robots@${config.rnl.domain}";
+      server = defaultACMEServer;
+    };
+    certs = lib.mkDefault {
+      "${config.networking.fqdn}" = {
+        extraDomainNames =
+          lib.mkIf
+          (config.networking.domain == config.rnl.domain)
+          ["${config.networking.hostName}.rnl.ist.utl.pt"];
+      };
+    };
+  };
+}
