7de709c3e9edb136700bf015a1d4b1e691cc40e1
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2023-08-28 17:06:38 +0100

    hosts/kitkat: add host

---
+++ hosts/kitkat.nix
@@ -0,0 +1,32 @@
+{profiles, ...}: {
+  imports = with profiles; [
+    core.rnl
+    filesystems.unknown
+    os.windows
+    type.vm
+  ];
+
+  rnl.labels.location = "chapek";
+
+  rnl.virtualisation.guest = {
+    description = "Servidor de Chocolatey e gestor de licenças do domínio WINRNL";
+
+    uefi = false;
+    memory = 8192;
+    vcpu = 8;
+
+    interfaces = [
+      {
+        source = "labs";
+        addressBus = "0x00";
+        addressSlot = "0x05";
+      }
+    ];
+    disks = [
+      {
+        type = "file";
+        source.file = "/mnt/data/lvm/kitkat.img";
+      }
+    ];
+  };
+}
