27d4e4da2dee4372431f4591d8b643f6d9a3e36c
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2023-08-28 17:14:05 +0100

    hosts/afs: add host

---
+++ hosts/afs/configuration.nix
@@ -0,0 +1,9 @@
+{lib, ...}: let
+in {
+  aliases = builtins.listToAttrs (builtins.map
+    (n: {
+      name = "afs${lib.fixedWidthNumber 2 n}";
+      value = {};
+    })
+    (lib.range 1 26));
+}
