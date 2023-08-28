46b48d1776fc3fbd00800ca780a800d67e426936
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2023-08-28 17:11:22 +0100

    hosts/peter: add host

---
+++ hosts/peter.nix
@@ -0,0 +1,39 @@
+{profiles, ...}: {
+  imports = with profiles; [
+    core.rnl
+    filesystems.unknown
+    os.windows
+    type.vm
+  ];
+
+  rnl.labels.location = "chapek";
+
+  rnl.virtualisation.guest = {
+    description = "Domain Controller do domínio DEIAD (Backup)";
+    maintainers = ["dei"];
+
+    uefi = false;
+    autostart = false; # Keep it off until starts working
+    memory = 8192;
+    vcpu = 4;
+
+    interfaces = [
+      {
+        source = "gia";
+        mac = "52:54:00:f9:ff:73";
+        addressBus = "0x00";
+        addressSlot = "0x05";
+      }
+    ];
+    disks = [
+      {
+        type = "file";
+        source.file = "/mnt/data/lvm/peter-root.img";
+      }
+      {
+        type = "file";
+        source.file = "/mnt/data/lvm/peter-data.img";
+      }
+    ];
+  };
+}
