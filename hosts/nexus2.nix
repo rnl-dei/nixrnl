d6a5e372eb44360cc7507df7c8de1556e8eca576
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2023-08-28 16:23:37 +0100

    hosts/nexus2: add host

---
+++ hosts/nexus2.nix
@@ -0,0 +1,68 @@
+{
+  profiles,
+  ...
+}: {
+  imports = with profiles; [
+    core.rnl
+    filesystems.simple-uefi
+    os.nixos
+    type.vm
+
+    fail2ban
+    ist-shell
+    nexus
+  ];
+
+  # Networking
+  networking.interfaces.enp1s0 = {
+    ipv4 = {
+      addresses = [
+        {
+          address = "193.136.164.131";
+          prefixLength = 26;
+        }
+      ];
+      routes = [
+        {
+          address = "0.0.0.0";
+          prefixLength = 0;
+          via = "193.136.164.190";
+        }
+      ];
+    };
+    ipv6 = {
+      addresses = [
+        {
+          address = "2001:690:2100:83::131";
+          prefixLength = 64;
+        }
+      ];
+      routes = [
+        {
+          address = "::";
+          prefixLength = 0;
+          via = "2001:690:2100:83::ffff:1";
+        }
+      ];
+    };
+  };
+
+  # Set as slave
+  services.keepalived.vrrpInstances = {
+    nexusIP4.priority = 254;
+    nexusIP6.priority = 254;
+  };
+
+  rnl.labels.location = "chapek";
+
+  rnl.virtualisation.guest = {
+    description = "Shell para alunos (Backup)";
+    createdBy = "nuno.alves";
+
+    memory = 8192;
+    vcpu = 8;
+
+    interfaces = [{source = "dmz";}];
+    disks = [{source.dev = "/dev/zvol/dpool/volumes/nexus2";}];
+  };
+}
