d9bf6a656e373151d1e245118a39ecad943999d8
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2023-08-28 17:09:40 +0100

    hosts/neiist: add host

---
+++ hosts/neiist.nix
@@ -0,0 +1,38 @@
+{profiles, ...}: {
+  imports = with profiles; [
+    core.rnl
+    filesystems.simple-uefi
+    os.ubuntu
+    type.vm
+  ];
+
+  rnl.labels.location = "zion";
+
+  rnl.virtualisation.guest = {
+    description = "Webserver do NEIIST";
+    maintainers = ["neiist"];
+
+    uefi = false;
+    memory = 4096;
+    vcpu = 2;
+
+    interfaces = [
+      {
+        source = "dmz";
+        mac = "52:54:00:ef:b5:13";
+        addressBus = "0x00";
+        addressSlot = "0x05";
+      }
+    ];
+    disks = [
+      {
+        type = "file";
+        source.file = "/mnt/data/lvm/neiist.img";
+      }
+      {
+        type = "file";
+        source.file = "/mnt/data/lvm/neiist-old.img";
+      }
+    ];
+  };
+}
