4ab655fdc23645f7e15720214e24bc37c56c60d9
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2023-08-28 15:31:51 +0100

    pkgs/mattermostPlugins: add playbooks and rssfeed plugins

---
+++ pkgs/mattermostPlugins/playbooks.nix
@@ -0,0 +1,5 @@
+{...}:
+builtins.fetchurl {
+  url = "https://github.com/mattermost/mattermost-plugin-playbooks/releases/download/v1.38.0/playbooks-v1.38.0.tar.gz";
+  sha256 = "sha256:156a2h4rbcnh7xsvpyffbkdrkxgzyalzkl0yfyiigk5mlfxhih14";
+}
