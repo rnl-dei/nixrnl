7d3093984ff2e1c0dba2ab026bf66eb017458890
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2023-08-29 10:16:23 +0100

    hosts/rates: add worker and backend host

---
+++ hosts/rates-worker.nix
@@ -0,0 +1,22 @@
+{profiles, ...}: {
+  imports = with profiles; [
+    core.third-party
+    filesystems.simple-uefi
+    os.ubuntu
+    type.vm
+  ];
+
+  rnl.labels.location = "zion";
+
+  rnl.virtualisation.guest = {
+    description = "Worker do sistema Rates - Tese do André Marinho";
+    createdBy = "diogo.cardoso";
+    maintainers = ["andre.marinho"];
+
+    memory = 4096;
+    vcpu = 4;
+
+    interfaces = [{source = "dmz";}];
+    disks = [{source.dev = "/dev/zvol/dpool/volumes/rates-worker";}];
+  };
+}
