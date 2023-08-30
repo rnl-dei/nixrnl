3ce46a909d0f656c789276521ec25ed306138241
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2023-08-30 11:37:18 +0100

    hosts/lab6: remove nvidia drivers

--- hosts/lab6/default.nix
+++ hosts/lab6/default.nix
@@ -1,4 +1,8 @@
-{profiles, ...}: {
+{
+  lib,
+  profiles,
+  ...
+}: {
   imports = with profiles; [
     core.rnl
     filesystems.labs
@@ -10,6 +14,10 @@
   ];
 
   rnl.storage.disks.root = ["/dev/sda"];
+
+  # To be able to use VNC we need to connect the monitor to the motherboard
+  # instead of the graphics card. So we need to disable the NVIDIA drivers.
+  services.xserver.videoDrivers = lib.mkForce ["modesetting" "fbdev"];
 
   rnl.labels.location = "inf1-p1-lab6";
 }
