7e64d7acb7b6230348aaaeae496cd2a922c192df
André Breda <andrebreda@tecnico.ulisboa.pt>
2023-09-04 15:35:11 +0100

    profiles/cluster: Only allow Slurm to run when cirrus is mounted

--- profiles/cluster/client.nix
+++ profiles/cluster/client.nix
@@ -3,5 +3,12 @@
 
   services.slurm.client.enable = true;
 
+  # Ensure slurmd does not run without /mnt/cirrus being mounted
+  systemd.services.slurmd = {
+    requires = ["mnt-cirrus.mount"];
+    after = ["mnt-cirrus.mount"];
+    partOf = ["mnt-cirrus.mount"];
+  };
+
   networking.firewall.allowedTCPPorts = [6818];
 }
