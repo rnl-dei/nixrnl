8ef609053a3a36bb9e46c54fdca0377d293b2899
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2023-09-07 02:24:41 +0100

    profiles/ist-shell: fix tasks max

--- profiles/ist-shell.nix
+++ profiles/ist-shell.nix
@@ -126,7 +126,7 @@
     sliceConfig = {
       # @ist189409's computer had ~1600 tasks in /user.slice
       # This ought to be enough to accomodate any not-too-unreasonable workload, while stopping fork bombs.
-      TasksMax = 4096;
+      TasksMax = lib.mkDefault 4096;
     };
 
     # user-.slice does not exist, the settings must be stored under user-.slice.d/overrides.conf (a "drop-in" file) for this to work.
