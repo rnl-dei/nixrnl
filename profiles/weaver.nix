15c687bd3cded46d5afdd7c31894f183bdfdc4ee
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2023-09-20 16:04:31 +0100

    profiles/weaver: add profile

---
+++ profiles/weaver.nix
@@ -0,0 +1,51 @@
+{
+  config,
+  lib,
+  pkgs,
+  profiles,
+  ...
+}: {
+  imports = with profiles; [
+    webserver
+    dokuwiki.wiki
+  ];
+
+  # Weaver
+  services.nginx.virtualHosts.weaver = {
+    default = true;
+    serverName = lib.mkDefault "weaver.${config.rnl.domain}";
+    enableACME = true;
+    addSSL = true;
+    root = "/var/www";
+    locations = {
+      "/" = {
+        root = let
+          configFile = pkgs.dashyConfig.weaver;
+          dashy = pkgs.dashy.override {inherit configFile;};
+        in "${dashy}/share/dashy";
+      };
+      "~ /(doku)?wiki" = {return = "301 $scheme://${config.services.nginx.virtualHosts.wiki.serverName}";};
+      "/raaas" = {tryFiles = "$uri $uri/ /index.php";};
+    };
+  };
+
+  services.nginx.virtualHosts.wiki = {
+    serverName = "wiki.${config.rnl.domain}";
+    enableACME = true;
+    addSSL = true;
+  };
+
+  services.keepalived = {
+    enable = lib.mkDefault true;
+    vrrpInstances.weaverIP4 = {
+      virtualRouterId = 88;
+      interface = lib.mkDefault "enp1s0";
+      virtualIps = [{addr = "193.136.164.88/26";}]; # weaver IPv4
+    };
+    vrrpInstances.weaverIP6 = {
+      virtualRouterId = 88;
+      interface = lib.mkDefault "enp1s0";
+      virtualIps = [{addr = "2001:690:2100:81::88/64";}]; # weaver IPv6
+    };
+  };
+}
