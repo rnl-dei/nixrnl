9e0836e3c30efaaf2cb121a16d7b5de9f9cf93d5
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2023-09-22 22:14:14 +0100

    profiles/lab5: change disks from sdd to nvme

--- hosts/lab5/default.nix
+++ hosts/lab5/default.nix
@@ -8,7 +8,8 @@
     labs
   ];
 
-  rnl.storage.disks.root = ["/dev/sda"];
+  rnl.storage.disks.root = ["/dev/nvme0n1"];
+  rnl.windows-labs.partition = "/dev/nvme0n1p2";
 
   rnl.labels.location = "inf1-p2-lab5";
 }
