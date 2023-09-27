020d00e9286a9216d11e2570d19a5613ff5a26cf
André Breda <andrebreda@tecnico.ulisboa.pt>
2023-09-27 10:07:23 +0100

    profiles/os/nixos: Set NIX_PATH with flake input paths

--- profiles/os/nixos.nix
+++ profiles/os/nixos.nix
@@ -16,6 +16,21 @@
       unstable.flake = inputs.unstable;
     };
 
+    # Set flake inputs as NIX_PATH channels.
+    # This allows nix-shell to work as if a channel exists with the exact copy of nixpkgs
+    # (or unstable) that is provided as a flake input. Without it, a user must somehow create
+    # channels or set NIX_PATH themselves to use legacy nix commands.
+    nixPath = [
+      "nixpkgs=${inputs.nixpkgs.outPath}"
+      "unstable=${inputs.unstable.outPath}"
+
+      # This entry exists in the original NIX_PATH but makes no sense in our machines,
+      # as /etc/nixos/configuration.nix does not even exist.
+      # "nixos-configuration=/etc/nixos/configuration.nix"
+
+      "/nix/var/nix/profiles/per-user/root/channels"
+    ];
+
     # Generally useful nix option defaults
     settings = {
       keep-outputs = true;
