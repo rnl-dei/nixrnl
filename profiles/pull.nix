4570e5ecb036c4e7596178fb961d2fcf37c36b6e
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2023-10-11 13:17:03 +0100

    profiles/pull: fix flake uri

--- profiles/pull.nix
+++ profiles/pull.nix
@@ -4,6 +4,6 @@
     dates = "04:00";
     randomizedDelaySec = "45min";
     persistent = true;
-    flake = "https://gitlab.rnl.tecnico.ulisboa.pt/rnl/nixrnl";
+    flake = "git+https://gitlab.rnl.tecnico.ulisboa.pt/rnl/nixrnl?ref=master";
   };
 }
