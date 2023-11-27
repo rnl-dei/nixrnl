7935fa56d7649cd778b30ce60a36ee5ee9051d82
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2023-11-27 15:38:32 +0000

    profiles/phpfpm: add profile

---
+++ profiles/phpfpm.nix
@@ -0,0 +1,27 @@
+{config, ...}: {
+  users.users.php = {
+    group = config.services.nginx.group;
+    isSystemUser = true;
+  };
+
+  services.phpfpm.pools.default = {
+    user = "php";
+    group = config.services.nginx.group;
+
+    settings = {
+      "pm" = "dynamic";
+      "pm.max_children" = 32;
+      "pm.start_servers" = 2;
+      "pm.min_spare_servers" = 2;
+      "pm.max_spare_servers" = 5;
+      "pm.max_requests" = 500;
+
+      "listen.owner" = config.services.nginx.user;
+      "listen.group" = config.services.nginx.group;
+    };
+  };
+
+  services.nginx.upstreams.php = {
+    servers."unix:${config.services.phpfpm.pools.default.socket}" = {};
+  };
+}
