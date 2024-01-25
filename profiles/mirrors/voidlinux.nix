d6e5c0d4b0c32da5e2726d73ec1c0e7298427470
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2024-01-25 15:02:36 +0000

    profiles/mirrors: remove voidlinux

--- profiles/mirrors/voidlinux.nix
+++
@@ -1,7 +0,0 @@
-{...}: {
-  rnl.ftp-server.mirrors.voidlinux = {
-    source = "rsync://repo-sync.voidlinux.org/voidlinux/";
-    target = "/mnt/data/ftp/pub/voidlinux";
-    timer = "*-*-* 14:15:00"; # Every day at 14:15
-  };
-}
