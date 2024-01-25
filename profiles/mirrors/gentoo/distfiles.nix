adf568bcbf68ad438e67f98b62ce9141938f5d3b
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2024-01-25 20:55:46 +0000

    profiles/mirrors: add gentoo-distfiles key

--- profiles/mirrors/gentoo/distfiles.nix
+++ profiles/mirrors/gentoo/distfiles.nix
@@ -1,4 +1,4 @@
-{...}: {
+{config, ...}: {
   rnl.ftp-server.mirrors.gentoo-distfiles = {
     #source = "gentoo@masterdistfiles.gentoo.org::gentoo";
     source = "rsync://193.136.164.6/pub/gentoo/gentoo-distfiles/";
@@ -6,9 +6,15 @@
     timer = "*-*-* 0..23:00:00"; # Every 30 minutes
     extraArgs = [
       "-D"
-      #"--password-file=" # FIXME: Add password file
+      # "--password-file=${config.age.secrets."gentoo-distfiles-ssh.key".path}" # FIXME: Uncomment when moving to production
       "--exclude THIS-FILE-SHOULD-NOT-BE-PUBLIC.txt"
       "--delete-excluded"
     ];
+  };
+
+  age.secrets."gentoo-distfiles-ssh.key" = {
+    file = ../../../secrets/gentoo-distfiles-ssh-key.age;
+    owner = config.rnl.ftp-server.mirrors.gentoo-distfiles.user;
+    group = config.rnl.ftp-server.mirrors.gentoo-distfiles.group;
   };
 }
