2bbe2b5694a1649eef088e3f29f0f210186fad3b
Diogo <diogo.cardoso@rnl.tecnico.ulisboa.pt>
2024-01-25 15:42:32 +0000

    profiles/mirrors/opensuse: also mirror tumbleweed

--- profiles/mirrors/opensuse.nix
+++ profiles/mirrors/opensuse.nix
@@ -1,6 +1,6 @@
 {...}: {
   rnl.ftp-server.mirrors.opensuse = {
-    #source = "rsync://stage.opensuse.org/opensuse-full/opensuse/";
+    #source = "rsync://stage.opensuse.org/opensuse-full-with-factory/opensuse/";
     source = "rsync://193.136.164.6/pub/opensuse";
     target = "/mnt/data/ftp/pub/opensuse";
     timer = "*-*-* 4..23/6:20:00"; # Every 6 hours between 4am and 11pm
