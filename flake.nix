41633a68c4577a3b8b1efc0a801e7377cd7d11c1
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2024-02-07 15:32:32 +0000

    flake.nix: bump nixpkgs 25.05->23.11

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "NixOS @ RNL";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
     unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
     # Comment out because it's not used
