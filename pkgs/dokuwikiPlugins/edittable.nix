84419cd8ecb81bb64bebfa1ad6ff627f23307e7f
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2024-02-07 20:11:07 +0000

    pkgs/dokuwikiPlugins: upgrade plugins

--- pkgs/dokuwikiPlugins/edittable.nix
+++ pkgs/dokuwikiPlugins/edittable.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation {
   name = "edittable";
   src = fetchzip {
     url = "https://github.com/cosmocode/edittable/archive/master.zip";
-    sha256 = "sha256-Mns8zgucpJrg1xdEopAhd4q1KH7j83Mz3wxuu4Thgsg=";
+    sha256 = "sha256-1TM7QkyNbDMQ/GR2PMKWPNHG3jhxdLNaqDbErltSsAs=";
   };
   installPhase = "mkdir -p $out; cp -R * $out/";
 }
--- pkgs/dokuwikiPlugins/wrap.nix
+++ pkgs/dokuwikiPlugins/wrap.nix
@@ -7,7 +7,7 @@ stdenv.mkDerivation {
   name = "wrap";
   src = fetchzip {
     url = "https://github.com/selfthinker/dokuwiki_plugin_wrap/archive/master.zip";
-    sha256 = "sha256-qIgyK6xNpo9Dr2tpgtInSjPBlkGr7EHzqrqhwHLtwNM=";
+    sha256 = "sha256-XVmrIUVD0Q6F8BXByhYd0bKtvVK22LLpijVXHTrZD2k=";
   };
   installPhase = "mkdir -p $out; cp -R * $out/";
 }
