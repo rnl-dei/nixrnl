b88c7f11fdf8c951fefe510604f33ea4a8d9dab8
Carlos Vaz <carlos.vaz@rnl.tecnico.ulisboa.pt>
2024-02-08 13:08:15 +0000

    hosts/dollars: add binary cache server

---
+++ hosts/dollars.nix
@@ -0,0 +1,72 @@
+{
+  config,
+  pkgs,
+  profiles,
+  ...
+}: {
+  imports = with profiles; [
+    core.rnl
+    filesystems.zfs-mirror
+    os.nixos
+    type.physical
+
+    binaryCache
+    webserver
+  ];
+
+  rnl.labels.location = "inf1-p2-c1";
+
+  # Storage
+  rnl.storage.disks.root = [
+    "/dev/disk/by-id/ata-WDC_WD1002FAEX-00Y9A0_WD-WCAW34582499"
+    "/dev/disk/by-id/ata-WDC_WD10EZEX-00RKKA0_WD-WCC1S3606587"
+  ];
+
+  # Networking
+  networking = {
+    hostId = "b0234a5e";
+
+    interfaces.enp0s31f6 = {
+      ipv4 = {
+        addresses = [
+          {
+            address = "193.136.154.116";
+            prefixLength = 25;
+          }
+        ];
+        routes = [
+          {
+            address = "0.0.0.0";
+            prefixLength = 0;
+            via = "193.136.154.126";
+          }
+        ];
+      };
+      ipv6 = {
+        addresses = [
+          {
+            address = "2001:690:2100:84::8000:116";
+            prefixLength = 64;
+          }
+        ];
+        routes = [
+          {
+            address = "::";
+            prefixLength = 0;
+            via = "fe80::96f7:ad00:28af:fa13";
+          }
+        ];
+      };
+    };
+  };
+
+  users.users.root.hashedPassword = "$6$FYer1DJhVX1eYtCx$/gMyf.WtjzxE/KyeGaHPLeeJN2.mMkCsPuKBGkCGlpxmhnJrh7nbqj.97aLmMM9vLpjWFj7FWkmNT9Q4jAlRG1";
+
+  age.secrets."dollars-binary-cache-key" = {
+    file = ../secrets/dollars-binary-cache-key.age;
+    owner = "harmonia";
+    group = "harmonia";
+  };
+
+  services.harmonia.signKeyPath = config.age.secrets."dollars-binary-cache-key".path;
+}
---
+++ profiles/binary-cache.nix
@@ -0,0 +1,29 @@
+{
+  config,
+  pkgs,
+  ...
+}: {
+  # Generate a public/private key pair like this:
+  # $ nix-store --generate-binary-cache-key cache.yourdomain.tld-1 /var/lib/secrets/harmonia.secret /var/lib/secrets/harmonia.pub
+  services.harmonia.enable = true;
+  nix.settings.allowed-users = ["harmonia"];
+
+  services.nginx = {
+    virtualHosts."dollars.rnl.tecnico.ulisboa.pt" = {
+      enableACME = true;
+      forceSSL = true;
+      locations."/".extraConfig = ''
+        proxy_pass http://127.0.0.1:5000;
+        proxy_set_header Host $host;
+        proxy_redirect http:// https://;
+        proxy_http_version 1.1;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header Upgrade $http_upgrade;
+        proxy_set_header Connection $connection_upgrade;
+
+        zstd on;
+        zstd_types application/x-nix-archive;
+      '';
+    };
+  };
+}
