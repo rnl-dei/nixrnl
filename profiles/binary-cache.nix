3647772b5f439f9819df427531822fa2c13cfc0f
Carlos Vaz <carlos.vaz@rnl.tecnico.ulisboa.pt>
2024-02-08 16:37:24 +0000

    fix formatting

--- profiles/binary-cache.nix
+++ profiles/binary-cache.nix
@@ -9,21 +9,24 @@
   nix.settings.allowed-users = ["harmonia"];
 
   services.nginx = {
+    # TODO https://nixos.wiki/wiki/FAQ/Private_Cache_Proxy
     virtualHosts."dollars.rnl.tecnico.ulisboa.pt" = {
       enableACME = true;
       forceSSL = true;
-      locations."/".extraConfig = ''
-        proxy_pass http://127.0.0.1:5000;
-        proxy_set_header Host $host;
-        proxy_redirect http:// https://;
-        proxy_http_version 1.1;
-        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
-        proxy_set_header Upgrade $http_upgrade;
-        proxy_set_header Connection $connection_upgrade;
+      locations = {
+        "/".extraConfig = ''
+          proxy_pass http://127.0.0.1:5000;
+          proxy_set_header Host $host;
+          proxy_redirect http:// https://;
+          proxy_http_version 1.1;
+          proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+          proxy_set_header Upgrade $http_upgrade;
+          proxy_set_header Connection $connection_upgrade;
 
-        zstd on;
-        zstd_types application/x-nix-archive;
-      '';
+          zstd on;
+          zstd_types application/x-nix-archive;
+        '';
+      };
     };
   };
 }
