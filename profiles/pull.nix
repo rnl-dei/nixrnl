34021bfc5232cd1b645d6ee3c10b8539c0c65e97
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2024-02-26 18:29:29 +0000

    profiles/pull: change flake uri to github

--- profiles/pull.nix
+++ profiles/pull.nix
@@ -4,6 +4,6 @@
     dates = "04:00";
     randomizedDelaySec = "45min";
     persistent = true;
-    flake = "git+https://gitlab.rnl.tecnico.ulisboa.pt/rnl/nixrnl?ref=master";
+    flake = "github:rnl-dei/nixrnl/master"; # Use GitLab mirror because the repository is private
   };
 }
