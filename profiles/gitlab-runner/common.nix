b3473700e2c6364b7dc2e36f903a19fb4369277a
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2024-02-26 13:30:07 +0000

    profiles/gitlab-runner: add support to podman

--- profiles/gitlab-runner/common.nix
+++ profiles/gitlab-runner/common.nix
@@ -1,5 +1,21 @@
-{...}: {
+{
+  config,
+  lib,
+  ...
+}: let
+  hasPodman = config.virtualisation.podman.enable;
+in {
   services.gitlab-runner = {
     enable = true;
+  };
+
+  # Support for podman
+  virtualisation.podman.dockerSocket.enable = true;
+  systemd.services = lib.mkIf hasPodman {
+    gitlab-runner = {
+      after = ["podman.service"];
+      requires = ["podman.service"];
+      serviceConfig.SupplementaryGroups = ["podman"];
+    };
   };
 }
