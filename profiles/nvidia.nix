1f136a41c91f81559509a3b8556dbed66e9aee8d
Martim Monis <martim.monis@rnl.tecnico.ulisboa.pt>
2024-02-28 12:41:56 +0000

    add nvidia exporter package and service

--- profiles/nvidia.nix
+++ profiles/nvidia.nix
@@ -42,4 +42,21 @@
       AutoDetect=nvml
     '')
   ];
+
+  systemd.services."nvidia_gpu_exporter" = {
+    description="NVIDIA GPU Exporter";
+    serviceConfig = {
+      Type="simple";
+      User="nvidia_gpu_exporter";
+      Group="nvidia_gpu_exporter";
+      DynamicUser = true;
+
+      ExecStart="${pkgs.prometheusExporters.nvidia}/bin/nvidia_gpu_exporter";
+      SyslogIdentifier="nvidia_gpu_exporter";
+      Restart="always";
+      RestartSec="1";
+    };
+    wantedBy=[ "multi-user.target" ];
+    after=[ "network-online.target" ];
+  };
 }
