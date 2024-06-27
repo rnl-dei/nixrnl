c61124004a7adf82e30d0b628406bef9fd141d35
André Breda <21-ist189409@users.noreply.gitlab.rnl.tecnico.ulisboa.pt>
2024-06-27 13:44:21 +0000

    hosts/dredd(cirrus): Trade reliability for performance
    The NFS Cirrus mount is meant to be a best-effort performant shared-storage option when using the cluster.
    Thus, it makes sense to allow the NFS server to immediately acknowledge writes even if they weren't yet sync-ed to physical media.

--- hosts/dredd.nix
+++ hosts/dredd.nix
@@ -120,10 +120,10 @@
     enable = true;
     # allow borg and labs to mount cirrus
     exports = ''
-      /mnt/data/cirrus 193.136.164.138(rw,sync,no_subtree_check,no_root_squash)
-      /mnt/data/cirrus 2001:690:2100:83::138(rw,sync,no_subtree_check,no_root_squash)
-      /mnt/data/cirrus 193.136.154.0/25(rw,sync,no_subtree_check,no_root_squash)
-      /mnt/data/cirrus 2001:690:2100:84::/64(rw,sync,no_subtree_check,no_root_squash)
+      /mnt/data/cirrus 193.136.164.138(rw,async,no_subtree_check,no_root_squash)
+      /mnt/data/cirrus 2001:690:2100:83::138(rw,async,no_subtree_check,no_root_squash)
+      /mnt/data/cirrus 193.136.154.0/25(rw,async,no_subtree_check,no_root_squash)
+      /mnt/data/cirrus 2001:690:2100:84::/64(rw,async,no_subtree_check,no_root_squash)
     '';
   };
   networking.firewall.allowedTCPPorts = [2049];
