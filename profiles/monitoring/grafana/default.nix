bc5c967e80871a5fc41980c2e9e76943f9831e18
Martim Monis <martim.monis@rnl.tecnico.ulisboa.pt>
2024-07-01 16:43:45 +0100

    monitoring: wip grafana provisioning

--- profiles/monitoring/grafana/default.nix
+++ profiles/monitoring/grafana/default.nix
@@ -47,19 +47,97 @@
     };
     provision = {
       enable = true;
-      datasources.path = ./datasources.yaml;
-      alerting = {
-        rules.path = ./alerts/rules;
-        # templates.path = ./alerts/templates;
-        # policies.path = ./alerts/policies;
-        # contactPoints.path = ./alerts/contacts-points.yaml;
-      };
       dashboards.settings.providers = [
         {
           name = "RNL-Dashboards";
           options.path = ./dashboards;
         }
       ];
+      alerting = {
+        rules.path = ./alerts/rules;
+        # templates.path = ./alerts/templates;
+        # policies.path = ./alerts/policies;
+        contactPoints.settings = {
+          apiVersion = 1;
+          contactPoints = [
+            {
+              name = "RNL Alertas (Mattermost)";
+              orgId = 1;
+              receivers = [{
+                uid = "t8qo_N-Vk";
+                type = "slack";
+                settings = {
+                  recipient = "alertas";
+                  text = "{{ template 'mattermost.alerts' . }}";
+                  title = "{{ template 'mattermost.default.title' . }}";
+                  url = "$__env{GRAFANA_MATTERMOST_WEBHOOK}";
+                };
+                disableResolveMessage = true;
+              }];
+            }
+            {
+              name = "RNL Infra (Mattermost)";
+              orgId = 1;
+              receivers = [{
+                uid = "BPY2_H-4k";
+                type = "slack";
+                settings = {
+                  recipient = "infra";
+                  text = "{{ template 'Only description' }}";
+                  url = "$__env{GRAFANA_MATTERMOST_WEBHOOK}";
+                };
+                disableResolveMessage = false;
+              }];
+            }
+            {
+              name = "RNL Infra (Email)";
+              orgId = 1;
+              receivers = [{
+                uid = "qDMyWDa4k";
+                type = "email";
+                settings = {
+                  addresses = ["infra-robots@rnl.tecnico.ulisboa.pt"];
+                  singleEmail = false;
+                };
+                disableResolveMessage = false;
+              }];
+            }
+          ];
+        };
+      };
+      datasources.settings.datasources = {
+        "Prometheus" = {
+          uid = "PBFA97CFB590B2093";
+          type = "prometheus";
+          url = "http://localhost:9090";
+          access = "proxy";
+          isDefault = true;
+        };
+        "Loki" = {
+          uid = "pxCqOS5Iz";
+          type = "loki";
+          url = "http://localhost:13100";
+          access = "proxy";
+        };
+        "MySQL" = {
+          uid = "y7lCbTU7z";
+          type = "mysql";
+          url = "db.rnl.tecnico.ulisboa.pt";
+          access = "proxy";
+          user = "$__env{GRAFANA_MYSQL_USER}";
+          password = "$__env{GRAFANA_MYSQL_PASSWORD}";
+          database = "$__env{GRAFANA_MYSQL_DATABASE}";
+        };
+        "MySQL Tardis" = {
+          uid = "d306f8d7-ec7c-418e-a6fa-c2106c3f23fd";
+          type = "mysql";
+          url = "localhost";
+          access = "proxy";
+          user = "$__env{GRAFANA_MYSQL_TARDIS_USER}";
+          password = "$__env{GRAFANA_MYSQL_TARDIS_PASSWORD}";
+          database = "$__env{GRAFANA_MYSQL_TARDIS_DATABASE}";
+        };
+      };
     };
   };
 
