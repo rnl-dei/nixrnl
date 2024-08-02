5e73e246aad8ec225974761b9c9acd8846696e3f
Vasco Guerreiro Vintém Morais <vasco.morais@rnl.tecnico.ulisboa.pt>
2024-08-02 12:57:21 +0100

    simplify network routes

--- hosts/dolly.nix
+++ hosts/dolly.nix
@@ -22,37 +22,24 @@
   ];
 
   # Networking
-  networking.interfaces.eno1 = {
-    ipv4 = {
-      addresses = [
+  networking = {
+    interfaces.eno1 = {
+      ipv4.addresses = [
         {
           address = "193.136.154.125";
           prefixLength = 25;
         }
       ];
-      routes = [
-        {
-          address = "0.0.0.0";
-          prefixLength = 0;
-          via = "193.136.154.126";
-        }
-      ];
-    };
-    ipv6 = {
-      addresses = [
+      ipv6.addresses = [
         {
           address = "2001:690:2100:84:8000::125";
           prefixLength = 64;
         }
       ];
-      routes = [
-        {
-          address = "::";
-          prefixLength = 0;
-          via = "fe80::96f7:ad00:28af:fa13";
-        }
-      ];
     };
+
+    defaultGateway.address = "193.136.154.126";
+    defaultGateway6.address = "fe80::96f7:ad00:28af:fa13";
   };
 
   users.users.root.hashedPassword = "$6$fBxN95kuNpq4Nxhq$Rev.mpIltLW7keZoT/LLtuamiggGTpBtfs.Z.8ztxin9TI9ZksIUNYeOvc4RgQF.n.nlGAlXRI4IBxyFj3VSa/";
