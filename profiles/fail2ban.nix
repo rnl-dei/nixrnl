758f30d5d83bab831229f0a32603dc099167bfba
Vasco Guerreiro Vintém Morais <vasco.morais@rnl.tecnico.ulisboa.pt>
2024-08-22 18:09:29 +0100

    profiles/fail2ban: fixed ipdb reports and added correct comments

--- profiles/fail2ban.nix
+++ profiles/fail2ban.nix
@@ -8,11 +8,11 @@
   inherit (config) services;
 in {
   age.secrets."abuseipdb-api-key".file = ../secrets/abuseipdb-api-key.age;
-
-  systemd.services.fail2ban.serviceConfig.EnvironmentFile = config.age.secrets."abuseipdb-api-key".path;
-
   services.fail2ban = {
     enable = true;
+
+    extraPackages = [ pkgs.curl ];
+
     ignoreIP = [
       # RNL IPs
       "193.136.164.0/24"
@@ -30,12 +30,21 @@ in {
       rndtime = "4m";
     };
 
+    package = pkgs.fail2ban.overrideAttrs (final: prev: {
+      preConfigure =
+        prev.preConfigure
+        + ''
+          sed -i 's|<abuseipdb_apikey>|$(cat ${config.age.secrets."abuseipdb-api-key".path})|' config/action.d/abuseipdb.conf
+          sed -i 's|\$lgm|<abuseipdb_comment>|' config/action.d/abuseipdb.conf
+        '';
+    });
+
     jails = {
       # postfix
       postfix = mkIf services.postfix.enable {
         settings = {
           filter = "postfix";
-          action = ''abuseipdb[abuseipdb_apikey=$ABUSEIPDB_KEY, abuseipdb_category="11,18", abuseipdb_comment="postfix (h)"]'';
+          action = ''abuseipdb[abuseipdb_category="11,18", abuseipdb_comment="postfix"]'';
         };
       };
       # courier
@@ -44,7 +53,7 @@ in {
       nginx-botsearch = mkIf services.nginx.enable {
         settings = {
           filter = "nginx-botsearch";
-          action = ''abuseipdb[abuseipdb_apikey=$ABUSEIPDB_KEY, abuseipdb_category="21", abuseipdb_comment="bot search (h)"]'';
+          action = ''abuseipdb[abuseipdb_category="21", abuseipdb_comment="bot search"]'';
         };
       };
 
@@ -53,7 +62,7 @@ in {
         settings = {
           filter = "php-url-fopen";
           maxretry = 1;
-          action = ''abuseipdb[abuseipdb_apikey=$ABUSEIPDB_KEY, abuseipdb_category="21", abuseipdb_comment="php f-open() abuse (h)"]'';
+          action = ''abuseipdb[abuseipdb_category="21", abuseipdb_comment="php f-open() abuse"]'';
         };
       };
 
@@ -61,7 +70,7 @@ in {
       sshd = mkIf services.openssh.enable {
         settings = {
           filter = "sshd";
-          action = ''abuseipdb[abuseipdb_apikey=%($ABUSEIPDB_KEY)s, abuseipdb_category="18,22", abuseipdb_comment="ssh abuse (h)"]'';
+          action = ''abuseipdb[abuseipdb_category="18,22", abuseipdb_comment="ssh abuse"]'';
         };
       };
     };
