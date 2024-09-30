5b0dd26c1fcfa0f5f3cee95d082b124ce57b0c36
Nuno Alves <nuno.alves@rnl.tecnico.ulisboa.pt>
2024-09-30 23:28:19 +0100

    hosts: add network hosts

---
+++ hosts/pdu/configuration.nix
@@ -0,0 +1,14 @@
+{...}: {
+  aliases = {
+    "mareep".extraModules = [
+      {
+        rnl.labels.location = "inf1-p01-a2";
+      }
+    ];
+    "flaaffy".extraModules = [
+      {
+        rnl.labels.location = "inf1-p01-a2";
+      }
+    ];
+  };
+}
