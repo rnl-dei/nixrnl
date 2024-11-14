ed41b98806373f471b7bc99ae4852f88cd505d4b
Francisco Martins <francisco.martins@rnl.tecnico.ulisboa.pt>
2024-11-14 12:02:02 +0000

    Start the config for kea.

--- profiles/doorstep.nix
+++ profiles/doorstep.nix
@@ -1,8 +1,17 @@
 { pkgs, ... }:
 {
   environment.systemPackages = [ pkgs.kea ];
-  # Configure Hashicorp Vault
+  # Kea Config
   services.kea.dhcp4 = {
     enable = true;
+    settings = {
+      subnet4 = [
+        {
+          pools = [ { pool = "10.16.80.1-10.16.80.4"; } ];
+          subnet = "10.16.80.0/23";
+        }
+      ];
+      valid-lifetime = 4000;
+    };
   };
 }
