3cf9fb5a30ec4c35c80b5e0881541249205de555
Francisco Martins <francisco.martins@rnl.tecnico.ulisboa.pt>
2024-11-15 10:41:56 +0000

    profiles: doorstep completed the base config

--- profiles/doorstep.nix
+++ profiles/doorstep.nix
@@ -5,13 +5,24 @@
   services.kea.dhcp4 = {
     enable = true;
     settings = {
+      lease-database = {
+        type = "memfile";
+        persist = true;
+        name = "/var/lib/kea/dhcp4.leases";
+      };
       subnet4 = [
         {
-          pools = [ { pool = "10.16.80.1-10.16.80.4"; } ];
+          pools = [ { pool = "10.16.80.5-10.16.80.250"; } ];
           subnet = "10.16.80.0/23";
         }
       ];
       valid-lifetime = 4000;
+      option-data = [
+        {
+          name = "DNS servers";
+          data = "ns.rnl.tecnico.ulisboa.pt";
+        }
+      ];
     };
   };
 }
