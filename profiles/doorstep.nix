963bcfa6504469db132859c5db71bb257e80c46b
Francisco-Andre-Martins <francisco.martins@rnl.tecnico.ulisboa.pt>
2024-11-15 22:06:53 +0000

    the config should now work maybe, hopefully

--- profiles/doorstep.nix
+++ profiles/doorstep.nix
@@ -12,6 +12,7 @@
       };
       subnet4 = [
         {
+          id = 1;
           pools = [ { pool = "10.16.80.5-10.16.80.250"; } ];
           subnet = "10.16.80.0/23";
         }
@@ -23,6 +24,16 @@
       };
       option-data = [
         {
+          name = "broadcast-address";
+          code = 28;
+          data = "10.16.81.255";
+        }
+        {
+          name = "routers";
+          code = 3;
+          data = "10.16.81.254";
+        }
+        {
           name = "domain-name-servers";
           code = 6;
           data = "193.136.164.1";
@@ -32,6 +43,12 @@
           code = 15;
           data = "rnl.tecnico.ulisboa.pt";
         }
+        {
+          name = "ntp-servers";
+          code = 42;
+          data = "193.136.164.4";
+        }
+
       ];
     };
   };
