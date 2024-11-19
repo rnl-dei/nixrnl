495fd9ef8425ebaa6f2e7be0c4a60250fcab4ee4
Vasco Guerreiro Vintém Morais <vasco.morais@rnl.tecnico.ulisboa.pt>
2024-11-19 12:41:10 +0000

    Resolve "Migrate doorstep to NixOS"

---
+++ hosts/doorstep1.nix
@@ -0,0 +1,34 @@
+{ profiles, ... }:
+{
+  imports = with profiles; [
+    core.rnl
+    filesystems.simple-uefi
+    os.nixos
+    type.vm
+    doorstep
+  ];
+
+  rnl.labels.location = "zion";
+
+  rnl.virtualisation.guest = {
+    description = "Servidor de dhcp da rede de portáteis.";
+    createdBy = "vasco.morais";
+
+    memory = 4096;
+    vcpu = 2;
+
+    interfaces = [ { source = "portateis"; } ];
+    disks = [ { source.dev = "/dev/zvol/dpool/data/doorstep"; } ];
+  };
+
+  networking = {
+    interfaces.enp1s0.ipv4.addresses = [
+      {
+        address = "10.16.81.251";
+        prefixLength = 23;
+      }
+    ];
+
+    defaultGateway.address = "10.16.81.254";
+  };
+}
