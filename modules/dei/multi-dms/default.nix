6f17a73f6358e26c59e4bbf1f64eda63b85c1da3
Rafael Girão <rafael.girao@rnl.tecnico.ulisboa.pt>
2024-11-19 13:42:49 +0000

    modules/dei/multi-dms: move caddy config to own file

--- modules/dei/multi-dms/default.nix
+++ modules/dei/multi-dms/default.nix
@@ -25,18 +25,6 @@ let
   systemd-escape = "${pkgs.systemdMinimal}/bin/systemd-escape";
   systemctl = "${pkgs.systemdMinimal}/bin/systemctl";
 
-  # FIXME: very ugly (bah!) ask rnl for a better way
-  hostv4 = (builtins.head config.networking.interfaces.enp1s0.ipv4.addresses).address;
-  hostv6 = (builtins.head config.networking.interfaces.enp1s0.ipv4.addresses).address;
-
-  # TODO:
-  # - handle production as well, not just blatta 
-  #    (i.e, handle the case of branch "master" differently:
-  #     - dont touch databases in production, dont create them at all, dont do caddy virtualhost, etc.
-  #     - 
-  #
-  # overall cleanup of this mess
-
   common = ''
     # max length for container name is 11
     # therefore, we use hash of branch name and trim to 7, 
@@ -425,80 +413,11 @@ in
       "d ${buildsDir} 0750 ${user} ${webserver.group} - -"
       "d ${instancesDir}/public 0750 ${user} ${webserver.group} - -"
       "d ${enabledSitesDir} 0750 ${user} ${webserver.group} - -"
-
-      # TODO: when does this run?
-      # what if it runs before the file exists...? :thinking:
-      # Make sure temporary selfsigned nixos CA cert is readable by Caddy 
-      "z /var/lib/acme/.minica 0755 acme acme -"
-      "z /var/lib/acme/.minica/cert.pem 0644 acme acme -"
     ];
 
-    services.nginx.defaultListenAddresses = [
-      "127.0.0.80"
-      # ipv6 has only one loopback address: using only ipv4.
-      # https://serverfault.com/questions/193377/ipv6-loopback-addresses-equivalent-to-127-x-x-x
-    ];
-
-    # TODO: caddy in its own module?
-    services.caddy = {
-      # TODO: remove `package` line on >= 25.11
-      # tls_trust_pool directive doesn't seem to exist atm (on 24.05's version)
-      package = pkgs.unstable.caddy;
-      enable = true;
-      acmeCA = config.security.acme.defaults.server;
-      inherit (config.security.acme.defaults) email;
-      globalConfig = ''
-        grace_period 10s
-        skip_install_trust
-        default_bind ${hostv4} [${hostv6}] #TODO: see comment at `hostv4`/6
-      '';
-      extraConfig = ''
-        import ${enabledSitesDir}/*
-      '';
-
-      # Eventually, make a real entry for production/master (or just let nginx keep handling it?)
-      # Note: the order of the handle directives matter!
-      # The first handle directive that matches will win.
-      virtualHosts =
-        {
-          # Gateway to redirect fenix oauth responses to correct DMS instance
-          "fenix-dms-gw.blatta.rnl.tecnico.ulisboa.pt".extraConfig = ''
-            encode zstd gzip
-            handle {
-              redir {header.Referer}login?{query}
-            }
-          '';
-        }
-        // (mapAttrs' (
-          _vhostName: vhostConfig:
-          let
-            #TODO: maybe explicitly set serverName for 'localhost' virtualHost (see changes made in webserver.nix) rather than doing this
-            svName =
-              if (vhostConfig.serverName != null) then
-                vhostConfig.serverName
-              else
-                "dummy-value-for-localhost.blatta.rnl.tecnico.ulisboa.pt"; # TODO: dont look too great, but why is there a magic 'localhost' in nginx's `virtualHosts` attribute????
-          in
-          {
-            #name = vhostConfig.serverName ? "dms.blatta.rnl.tecnico.ulisboa.pt";
-            name = svName;
-            # https://caddyserver.com/docs/caddyfile/directives/reverse_proxy#https
-            value = {
-              extraConfig = ''
-                encode zstd gzip
-                reverse_proxy https://127.0.0.80 {
-                  transport http {
-                    tls_server_name ${svName}
-                    tls_trust_pool file {
-                      pem_file /var/lib/acme/.minica/cert.pem /etc/ssl/certs/ca-bundle.crt
-                    }
-                  }
-                }
-              '';
-            };
-          }
-        ) config.services.nginx.virtualHosts);
-    };
+    services.caddy.extraConfig = ''
+      import ${enabledSitesDir}/*
+    '';
 
     systemd.services."multi-dms@" = {
       description = "DEI Management System Backend (%i)";
@@ -509,7 +428,7 @@ in
         nixos-container
 
         # Dependencies of nixos-container:
-        # TODO: this should be upstreamed, perhaps
+        # TODO: this should be upstreamed to nixpkgs, perhaps
         util-linux # dependency of nixos-container (e.g, it tries to do `exec (...) mountpoint`)
         systemd # dependency of nixos-container (and possibly prestart and poststop scripts)
         nix # nix-env
