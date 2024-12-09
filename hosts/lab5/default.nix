7cb3bb940277d9c855c4ec0fd6014cfea5d2b610
Carlos Vaz <carlos.vaz@rnl.tecnico.ulisboa.pt>
2024-12-09 10:19:15 +0000

    lab5: disable pcie_aspm to fix 10 Mbps

--- hosts/lab5/default.nix
+++ hosts/lab5/default.nix
@@ -13,6 +13,8 @@
   rnl.storage.disks.root = [ "/dev/nvme0n1" ];
   rnl.windows-labs.partition = "/dev/nvme0n1p2";
 
+  boot.kernelParams = [ "pcie_aspm=off" ];
+
   rnl.labels.location = "inf1-p2-lab5";
 
   rnl.monitoring.amt = true;
