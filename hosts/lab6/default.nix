dd37127e20132cb437be5748a4a4390d53e09bfa
Carlos Vaz <carlos.vaz@rnl.tecnico.ulisboa.pt>
2024-12-09 10:18:46 +0000

    lab6: disable nvidia

--- hosts/lab6/default.nix
+++ hosts/lab6/default.nix
@@ -1,4 +1,4 @@
-{ lib, profiles, ... }:
+{ profiles, ... }:
 {
   imports = with profiles; [
     core.rnl
@@ -8,18 +8,10 @@
 
     labs
     cluster.client
-    nvidia
   ];
 
   rnl.storage.disks.root = [ "/dev/nvme0n1" ];
   rnl.windows-labs.partition = "/dev/nvme0n1p2";
-
-  # To be able to use VNC we need to connect the monitor to the motherboard
-  # instead of the graphics card. So we need to disable the NVIDIA drivers.
-  services.xserver.videoDrivers = lib.mkForce [
-    "modesetting"
-    "fbdev"
-  ];
 
   rnl.labels.location = "inf1-p1-lab6";
 
