c19dcbd3a5afdc2f02032fc684c22a23b776d905
Rafael Girão <rafael.girao@rnl.tecnico.ulisboa.pt>
2025-01-10 10:57:19 +0000

    pkgs/moodlePlugins/peerwork: fix src url

--- pkgs/moodlePlugins/peerwork.nix
+++ pkgs/moodlePlugins/peerwork.nix
@@ -1,12 +1,13 @@
 { fetchzip, moodle-utils, ... }:
 moodle-utils.buildMoodlePlugin rec {
   name = "peerwork";
-  version = "2023062000";
+  version = "2024122401";
 
   pluginType = "mod";
 
   src = fetchzip {
-    url = "https://moodle.org/plugins/download.php/29492/${pluginType}_${name}_moodle42_${version}.zip";
-    sha256 = "sha256-XkvckkhUb4zr9R8d4F+Feb14RialMXfWQgO5s58hpAA=";
+    url = "https://moodle.org/plugins/download.php/34326/${pluginType}_${name}_moodle42_${version}.zip";
+    sha256 = "sha256-asokTbWf+wKYLmeVzensuKLdBwTKgIFJdwKKTb8UlCs=";
   };
+
 }
