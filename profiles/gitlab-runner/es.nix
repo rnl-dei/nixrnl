df7cfeef2c8905ef7a04f5eea67eb2c7eccd9651
TIAGO CAIXINHA <tiago.caixinha@tecnico.ulisboa.pt>
2025-02-18 22:59:18 +0000

    hosts/lab6: added gitlab runner for ES25 on lab6; profiles/gitlab-runner: update for gitlab 17.0 compatibility; secrets: update es secret name

--- profiles/gitlab-runner/es.nix
+++ profiles/gitlab-runner/es.nix
@@ -6,7 +6,7 @@
     services = {
       es = {
         dockerImage = "ubuntu:23.10";
-        registrationConfigFile = config.age.secrets."gl-runner-es.env".path;
+        authenticationTokenConfigFile = config.age.secrets."gl-runner-es.env".path;
         registrationFlags = [ "--output-limit=16384" ];
         description = "gitlab-runner-es";
       };
@@ -14,6 +14,6 @@
   };
 
   age.secrets."gl-runner-es.env" = {
-    file = ../../secrets/gitlab-runners/es-24-env.age;
+    file = ../../secrets/gitlab-runners/es-25-env.age;
   };
 }
