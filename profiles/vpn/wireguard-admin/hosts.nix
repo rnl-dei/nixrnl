39db5c7ec09e803b9fedb9d50e7f5a3698fa4595
Vasco Guerreiro Vintém Morais <vasco.morais@rnl.tecnico.ulisboa.pt>
2025-02-20 11:05:47 +0000

    profiles/core/dei: add @ist1109803 ssh key; profiles/vpn/wireguard-admin: change @ist1109803 key; hosts/hagrid: add @ist1109803 workstation

--- profiles/vpn/wireguard-admin/hosts.nix
+++ profiles/vpn/wireguard-admin/hosts.nix
@@ -1,5 +1,5 @@
 # Please update this number when adding a new user
-# Last octet used: 78
+# Last octet used: 80
 [
   {
     # André Aparício
@@ -391,5 +391,10 @@
     # Simão Sanguinho
     publicKey = "5AZu5F+jiG9BEhyUZgvFzNucjAH7IwDG+Ardlfm9ECA=";
     lastOctet = 79;
+  }
+  {
+    # Hugo Pereira
+    publicKey = "pSzprPKBUujxDkPSVVnFnquqh8i7ym63APd3oF0s00A=";
+    lastOctet = 80;
   }
 ]
