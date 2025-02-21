5fa3047240e8c432806764ded3e4aa9d98407c48
Tiago Caixinha <tiago.caixinha@rnl.tecnico.ulisboa.pt>
2025-02-21 16:25:21 +0000

    fix es runners missing socket path

--- profiles/gitlab-runner/es.nix
+++ profiles/gitlab-runner/es.nix
@@ -7,7 +7,7 @@
       es = {
         dockerImage = "ubuntu:23.10";
         authenticationTokenConfigFile = config.age.secrets."gl-runner-es.env".path;
-        registrationFlags = [ "--output-limit=16384" ];
+        registrationFlags = [ "--output-limit=16384 --docker-host unix:///var/run/podman/podman.sock" ];
         description = "gitlab-runner-es";
       };
     };
