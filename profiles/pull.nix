a8d4f8d4d149e7d3e03e9e658f252117451a3155
Tiago Caixinha <tiago.caixinha@rnl.tecnico.ulisboa.pt>
2025-02-22 18:36:24 +0000

    profiles/pull: pull configs from our gitlab instance

--- profiles/pull.nix
+++ profiles/pull.nix
@@ -5,6 +5,6 @@
     dates = "04:00";
     randomizedDelaySec = "45min";
     persistent = true;
-    flake = "github:rnl-dei/nixrnl/master"; # Use GitLab mirror because the repository is private
+    flake = "gitlab:rnl/nixrnl/master?host=gitlab.rnl.tecnico.ulisboa.pt"; # Use our GitLab directly
   };
 }
