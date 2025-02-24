a8ef86fb9103fb08165d8426d958f42bb03f3c81
Vasco Guerreiro Vintém Morais <vasco.morais@rnl.tecnico.ulisboa.pt>
2025-02-24 11:54:18 +0000

    hosts: add hedgedoc; profiles: add hedgedoc

---
+++ hosts/hedgedoc.nix
@@ -0,0 +1,45 @@
+{ profiles, ... }:
+{
+  imports = with profiles; [
+    core.rnl
+    filesystems.simple-uefi
+    os.nixos
+    type.vm
+    hedgedoc
+  ];
+
+  # Networking
+  networking.interfaces.enp1s0 = {
+    ipv4.addresses = [
+      {
+        address = "193.136.164.109";
+        prefixLength = 26;
+      }
+    ];
+    ipv6.addresses = [
+      {
+        address = "2001:690:2100:81::109";
+        prefixLength = 64;
+      }
+    ];
+    defaultGateway.address = "193.136.164.126";
+    defaultGateway6.address = "2001:690:2100:81::ffff:1";
+  };
+
+  rnl.labels.location = "dredd";
+
+  rnl.storage.disks.data = [ "/dev/vdb" ];
+
+  rnl.virtualisation.guest = {
+    description = "Hedgedoc machine";
+    createdBy = "vasco.morais";
+
+    vcpu = 1;
+
+    interfaces = [ { source = "priv"; } ];
+    disks = [
+      { source.dev = "/dev/zvol/dpool/volumes/hedgedoc"; }
+      { source.dev = "/dev/zvol/dpool/data/hedgedoc"; }
+    ];
+  };
+}
---
+++ profiles/hedgedoc.nix
@@ -0,0 +1,9 @@
+{ }:
+{
+  services.hedgedoc = {
+    enable = true;
+    settings = {
+      dbURL = "algo@algo"; # probably will error out.
+    };
+  };
+}
