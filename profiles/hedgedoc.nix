1f502bf59814f679d561357c83e70f6d15aff9d6
Francisco-Andre-Martins <francisco.martins@rnl.tecnico.ulisboa.pt>
2025-02-27 21:37:21 +0000

    almost the secrets

--- profiles/hedgedoc.nix
+++ profiles/hedgedoc.nix
@@ -5,6 +5,13 @@
     CMD_HOST=localhost
     CMD_ALLOW_EMAIL_REGISTER=false
     CMD_EMAIL=false
+    CMD_OAUTH2_USER_PROFILE_USERNAME_ATTR=username
+    CMD_OAUTH2_USER_PROFILE_DISPLAY_NAME_ATTR=displayName
+    CMD_OAUTH2_TOKEN_URL=https://fenix.tecnico.ulisboa.pt/oauth/access_token
+    CMD_OAUTH2_AUTHORIZATION_URL=https://fenix.tecnico.ulisboa.pt/oauth/userdialog
+    CMD_OAUTH2_CLIENT_ID=288540197912778
+    CMD_OAUTH2_PROVIDERNAME=Fénix
+    CMD_OAUTH2_CLIENT_SECRET=${}
   '';
   /**
       hegdedoc oauth stuff
