4327fe0eba4a97f588e589caab1765e1d55bb2a5
Francisco-Andre-Martins <francisco.martins@rnl.tecnico.ulisboa.pt>
2025-03-07 16:37:22 +0000

    profiles/hedgedoc: almost ready

--- profiles/hedgedoc.nix
+++ profiles/hedgedoc.nix
@@ -1,27 +1,35 @@
 { config, pkgs, ... }:
 {
-  age.secrets."hedgedoc-gitlab-api" = {
-    file = ../secrets/hedgedoc-gitlab-api.age;
+  age.secrets."hedgedoc-fenix-api" = {
+    file = ../secrets/hedgedoc-fenix-api.age;
     owner = "hedgedoc";
   };
 
-  #decrypted = pkgs.agenix.decryptFile config.age.secrets."hedgedoc-gitlab-api".path;
+  #decrypted = pkgs.agenix.decryptFile config.age.secrets."hedgedoc-fenix-api".path;
   environment.etc."hedgedoc.env".text = ''
+    NODE_ENV=production
+    CMD_PROTOCOL_USESSL=true
     CMD_PORT=3000
     CMD_DOMAIN=hedgedoc.rnl.tecnico.ulisboa.pt
     CMD_HOST=localhost
     CMD_ALLOW_EMAIL_REGISTER=false
     CMD_EMAIL=false
     CMD_ALLOW_ANONYMOUS=false
-    CMD_GITLAB_BASEURL=https://gitlab.rnl.tecnico.ulisboa.pt
-    CMD_GITLAB_CLIENTID=96efa89c100470dfea13880ad599317d98da78e7ef15fe0946a91ce6a070a606
-    CMD_GITLAB_CLIENTSECRET=@secret@
-
+    CMD_OAUTH2_USER_PROFILE_USERNAME_ATTR=username
+    CMD_OAUTH2_USER_PROFILE_DISPLAY_NAME_ATTR=username
+    CMD_OAUTH2_USER_PROFILE_EMAIL_ATTR=email
+    CMD_OAUTH2_TOKEN_URL=https://fenix.tecnico.ulisboa.pt/oauth/access_token
+    CMD_OAUTH2_AUTHORIZATION_URL=https://fenix.tecnico.ulisboa.pt/oauth/userdialog
+    CMD_OAUTH2_CLIENT_ID=288540197912778
+    CMD_OAUTH2_PROVIDERNAME=Fénix
+    CMD_OAUTH2_CLIENT_SECRET=@secret@
     NODE_EXTRA_CA_CERTS=/etc/ssl/certs/ca-bundle.crt
+    CMD_OAUTH2_SCOPE=openid email profile
+    CMD_OAUTH2_USER_PROFILE_URL=https://fenix.tecnico.ulisboa.pt/api/fenix/v1/person
   '';
 
-  system.activationScripts."hedgedoc-gitlab-api" = ''
-    secret=$(cat "${config.age.secrets."hedgedoc-gitlab-api".path}")
+  system.activationScripts."hedgedoc-fenix-api" = ''
+    secret=$(cat "${config.age.secrets."hedgedoc-fenix-api".path}")
     configFile=/etc/hedgedoc.env
     ${pkgs.gnused}/bin/sed -i "s#@secret@#$secret#" "$configFile"
   '';
