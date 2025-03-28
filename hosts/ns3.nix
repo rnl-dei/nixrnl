c262862aeac4d604f9c02a690cf6cd4fd6029202
Francisco Martins <francisco.martins@rnl.tecnico.ulisboa.pt>
2025-03-28 21:13:57 +0000

    hosts/ns3, profiles/ns: initial commit

---
+++ hosts/ns3.nix
@@ -0,0 +1,49 @@
+{ profiles, ... }:
+{
+  imports = with profiles; [
+    core.rnl
+    filesystems.simple-uefi
+    os.nixos
+    type.vm
+  ];
+  rnl.virtualisation.guest = {
+    description = "Name Server";
+    createdBy = "francisco.martins";
+
+    vcpu = 1;
+    memory = 2048;
+    interfaces = [ { source = "pub"; } ];
+    disks = [
+      #{ source.dev = "/dev/zvol/dpool/volumes/hedgedoc"; }
+      { source.dev = "/dev/zvol/dpool/data/ns3"; }
+    ];
+  };
+
+  # Networking
+  #TODO
+  networking = {
+    defaultGateway.address = "193.136.164.62";
+    defaultGateway6.address = "2001:690:2100:80::ffff:1";
+
+    interfaces.enp1s0 = {
+      ipv4.addresses = [
+        {
+          address = "193.136.164.26";
+          prefixLength = 26;
+        }
+      ];
+      ipv6.addresses = [
+        {
+          address = "2001:690:2100:80::26";
+          prefixLength = 64;
+        }
+      ];
+
+    };
+  };
+
+  rnl.labels.location = "dredd";
+
+  #  rnl.storage.disks.data = [ "/dev/vdb" ];
+
+}
