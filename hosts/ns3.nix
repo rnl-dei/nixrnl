66528c1440b77acde350068dab371eaf0ae7779e
Francisco Martins <francisco.martins@rnl.tecnico.ulisboa.pt>
2025-04-08 18:30:21 +0100

    skill issue

--- hosts/ns3.nix
+++ hosts/ns3.nix
@@ -44,13 +44,11 @@
   };
 
   rnl.labels.location = "dredd";
-  users.users.root.
   #  rnl.storage.disks.data = [ "/dev/vdb" ];
-  age.secrets."root-at-ns3-ssh.key" =
-    {
-      file = ../secrets/root-at-ns3-ssh-key.age;
-      path = "/root/.ssh/id_ed25519";
-      owner = "root";
-    };
+  age.secrets."root-at-ns3-ssh.key" = {
+    file = ../secrets/root-at-ns3-ssh-key.age;
+    path = "/root/.ssh/id_ed25519";
+    owner = "root";
+  };
 
 }
