6699f0037f33a213378e93ea7f393fee564d2f5a
Francisco Martins <francisco.martins@rnl.tecnico.ulisboa.pt>
2025-04-11 19:23:08 +0100

    weird issues with the new ns

--- profiles/ns/slave.nix
+++ profiles/ns/slave.nix
@@ -16,6 +16,11 @@
         file = "/var/lib/slave-dns-config/rnl.tecnico.ulisboa.pt";
         masters = [ "193.136.164.1" ];
       };
+      zones."rnl.ist.utl.pt" = {
+        master = false;
+        masters = ["193.136.164.1"];
+        file = "/var/lib/slave-dns-config/rnl.tecnico.ulisboa.pt";
+      };
     };
   };
 }
