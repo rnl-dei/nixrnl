20a15656d39fe8782dde01d51ba3cd4867934491
Francisco Martins <francisco.martins@rnl.tecnico.ulisboa.pt>
2025-04-21 10:09:27 +0100

    finally it works

--- profiles/ns/generic.nix
+++ profiles/ns/generic.nix
@@ -23,7 +23,7 @@
     "10.16.80.0/20" # IPs privados IST da RNL
   ];
   age.secrets."root-at-ns-ssh.key" = {
-    file = ../secrets/root-at-ns-ssh-key.age;
+    file = ../../secrets/root-at-ns-ssh-key.age;
     path = "/root/.ssh/id_ed25519";
     owner = "root";
   };
