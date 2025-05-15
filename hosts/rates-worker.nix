ae975069bc830b8703666460a035e9779d850340
Francisco Martins <francisco.martins@rnl.tecnico.ulisboa.pt>
2025-05-15 10:07:43 +0100

    hosts/rates-backend, rates-worker: initial config

---
+++ hosts/rates-worker.nix
@@ -0,0 +1,28 @@
+{ profiles, ... }:
+{
+  imports = with profiles; [
+    core.third-party
+    filesystems.simple-uefi
+    os.ubuntu
+    type.vm
+  ];
+
+  rnl.labels.location = "dredd";
+
+  rnl.virtualisation.guest = {
+    description = "Backend do sistema RATES";
+    createdBy = "francisco.martins";
+    maintainers = [ "luis.macorano" ];
+
+    memory = 2048;
+    vcpu = 2;
+
+    interfaces = [
+      {
+        source = "pub";
+        mac = "52:54:00:51:b3:11";
+      }
+    ];
+    disks = [ { source.dev = "/dev/zvol/dpool/data/rates-worker"; } ];
+  };
+}
