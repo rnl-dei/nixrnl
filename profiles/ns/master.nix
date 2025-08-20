9781f0109202715fa54ad2392051a68845a3a17e
Francisco Martins <francisco.martins@rnl.tecnico.ulisboa.pt>
2025-08-20 21:44:12 +0100

    improving ns1 performance?

--- profiles/ns/master.nix
+++ profiles/ns/master.nix
@@ -23,52 +23,42 @@
       enable = true;
       zones."rnl.tecnico.ulisboa.pt" = {
         master = true;
-        slaves = [ "193.136.164.2" ];
         file = "/var/lib/dns-config/rnl.tecnico.ulisboa.pt";
       };
       zones."rnl.ist.utl.pt" = {
         master = true;
-        slaves = [ "193.136.164.2" ];
         file = "/var/lib/dns-config/rnl.tecnico.ulisboa.pt";
       };
       zones."89.16.10.in-addr.arpa" = {
         master = true;
-        slaves = [ "193.136.164.2" ];
         file = "/var/lib/dns-config/10.16.89.zone";
       };
       zones."86.16.10.in-addr.arpa" = {
         master = true;
-        slaves = [ "193.136.164.2" ];
         file = "/var/lib/dns-config/10.16.86.zone";
       };
       zones."82.16.10.in-addr.arpa" = {
         master = true;
-        slaves = [ "193.136.164.2" ];
         file = "/var/lib/dns-config/10.16.82.zone";
       };
       zones."81.16.10.in-addr.arpa" = {
         master = true;
-        slaves = [ "193.136.164.2" ];
         file = "/var/lib/dns-config/10.16.81.zone";
       };
       zones."80.16.10.in-addr.arpa" = {
         master = true;
-        slaves = [ "193.136.164.2" ];
         file = "/var/lib/dns-config/10.16.80.zone";
       };
       zones."154.136.193.in-addr.arpa" = {
         master = true;
-        slaves = [ "193.136.164.2" ];
         file = "/var/lib/dns-config/193.136.154.zone";
       };
       zones."164.136.193.in-addr.arpa" = {
         master = true;
-        slaves = [ "193.136.164.2" ];
         file = "/var/lib/dns-config/193.136.164.zone";
       };
       zones."8.0.0.0.0.1.2.0.9.6.0.1.0.0.2.ip6.arpa" = {
         master = true;
-        slaves = [ "193.136.164.2" ];
         file = "/var/lib/dns-config/2001:690:2100:8.zone";
       };
 
