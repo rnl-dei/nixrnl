72c0a885119a10ef445d347c796ea859ba0699e5
Vasco Petinga <vasco.petinga@rnl.tecnico.ulisboa.pt>
2025-08-27 00:57:16 +0100

    timer fix

--- profiles/mirrors/gentoo/portage.nix
+++ profiles/mirrors/gentoo/portage.nix
@@ -4,6 +4,6 @@
     #source = "rsync://masterportage.gentoo.org/gentoo-portage";
     source = "rsync://193.136.164.6/pub/gentoo/gentoo-portage";
     target = "/mnt/data/ftp/pub/gentoo/gentoo-portage";
-    timer = "4hours"; # NOTE: Confirm working
+    timer = "*-*-* 0/4:00:00"; # every 4 hours
   };
 }
