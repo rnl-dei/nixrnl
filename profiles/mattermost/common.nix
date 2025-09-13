c0de536756406102cf8c1045e4dea3be20a44277
Francisco Martins <francisco.martins@rnl.tecnico.ulisboa.pt>
2025-09-13 17:08:09 +0000

    Mattermost fixes

--- profiles/mattermost/common.nix
+++ profiles/mattermost/common.nix
@@ -10,13 +10,13 @@
     package = pkgs.unstable.mattermost;
     siteName = lib.mkDefault "Mattermost @ RNL";
     siteUrl = lib.mkDefault "https://${config.networking.fqdn}";
-    listenAddress = "127.0.0.1";
-    port = "8065";
-    localDatabaseCreate = lib.mkForce false;
+    host = "127.0.0.1";
+    port = 8065;
+    database.create = lib.mkForce false;
   };
 
   services.nginx.upstreams.mattermost.servers = {
-    "${config.services.mattermost.listenAddress}" = { };
+    "${config.services.mattermost.host}" = { };
   };
 
   services.nginx.virtualHosts.mattermost = {
@@ -25,10 +25,10 @@
     forceSSL = true;
     locations = {
       "/" = {
-        proxyPass = "http://mattermost";
+        proxyPass = "http://127.0.0.1:${toString config.services.mattermost.port}";
       };
       "~ /api/v[0-9]+/(users/)?websocket$" = {
-        proxyPass = "http://mattermost";
+        proxyPass = "http://127.0.0.1:${toString config.services.mattermost.port}";
         proxyWebsockets = true;
         extraConfig = ''
           client_max_body_size 100M;
