843207a3a92d282f00bcf2beba6aa4cb38227912
Vasco Guerreiro Vintém Morais <vasco.morais@rnl.tecnico.ulisboa.pt>
2025-10-23 13:20:17 +0100

    profiles/graphical/dashboard: use firefox

--- profiles/graphical/dashboard.nix
+++ profiles/graphical/dashboard.nix
@@ -5,7 +5,7 @@
   # Create RNL user without full permissions
   users.users.rnl = {
     isNormalUser = true;
-    description = "RNL user to run chromium and other stuff";
+    description = "RNL user to run firefox and other stuff";
   };
 
   services.xserver = {
@@ -27,23 +27,55 @@
   };
 
   xdg.autostart.enable = true;
-  environment.etc.autostartChromium = {
-    source = "${pkgs.chromium}/share/applications/chromium-browser.desktop";
-    target = "xdg/autostart/chromium-browser.desktop";
+
+  environment.etc."xdg/autostart/firefox.desktop".text = ''
+    [Desktop Entry]
+    Type=Application
+    Name=Firefox
+    Exec=${pkgs.firefox}/bin/firefox --kiosk
+  '';
+
+  environment.systemPackages = with pkgs; [
+    firefox
+    kdePackages.konsole
+  ]; # keep terminal installed to reopen programs in hagrid
+
+  services.unclutter-xfixes = {
+    enable = true;
+    timeout = 10;
   };
 
-  environment.systemPackages = with pkgs; [ chromium ];
-
-  programs.chromium = {
+  programs.firefox = {
     enable = true;
-    extensions = [
-      "pjgjpabbgnnoohijnillgbckikfkbjed" # Tab Rotate
-      "cjpalhdlnbpafiamejdnhcphjbkeiagm" # uBlock Origin
-    ];
-    extraOpts = {
-      "BrowserSignin" = 0;
-      "SyncDisabled" = true;
-      "PasswordManagerEnabled" = false;
+    policies = {
+      DisableFirefoxStudies = true;
+      DisableFirefoxAccounts = true;
+      DisableFirefoxScreenshots = true;
+      DisableForgetButton = true;
+      DisableMasterPasswordCreation = true;
+      DisableProfileImport = true;
+      DisableProfileRefresh = true;
+      DisableSetDesktopBackground = true;
+      DisablePocket = true;
+      DisableTelemetry = true;
+      DisableFormHistory = true;
+      DisablePasswordReveal = true;
+      TranslateEnabled = false;
+
+      ExtensionSettings =
+        let
+          moz = short: "https://addons.mozilla.org/firefox/downloads/latest/${short}/latest.xpi";
+        in
+        {
+          "uBlock0@raymondhill.net" = {
+            install_url = moz "ublock-origin";
+            installation_mode = "force_installed";
+          };
+          "tabrotator@davidfichtmueller.de" = {
+            install_url = moz "tab-rotator";
+            installation_mode = "force_installed";
+          };
+        };
     };
   };
 }
