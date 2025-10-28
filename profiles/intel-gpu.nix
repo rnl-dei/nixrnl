015fd0c26a24de397398fdbc3ba364685e9afa04
Francisco Martins <francisco.martins@rnl.tecnico.ulisboa.pt>
2025-10-28 15:18:05 +0000

    profiles/intel-gpu.nix: formatting

--- profiles/intel-gpu.nix
+++ profiles/intel-gpu.nix
@@ -1,24 +1,21 @@
 {
   pkgs,
-  config,
-  inputs,
-  lib,
   ...
-}:{
+}:
+{
   hardware.graphics = {
     enable = true;
     extraPackages = with pkgs; [
       # Required for modern Intel GPUs (Xe iGPU and ARC)
-      intel-media-driver     # VA-API (iHD) userspace
-      vpl-gpu-rt             # oneVPL (QSV) runtime
-
+      intel-media-driver # VA-API (iHD) userspace
+      vpl-gpu-rt # oneVPL (QSV) runtime
       # Optional (compute / tooling):
-      intel-compute-runtime  # OpenCL (NEO) + Level Zero for Arc/Xe
+      intel-compute-runtime # OpenCL (NEO) + Level Zero for Arc/Xe
     ];
   };
 
   environment.sessionVariables = {
-    LIBVA_DRIVER_NAME = "iHD";     # Prefer the modern iHD backend
+    LIBVA_DRIVER_NAME = "iHD"; # Prefer the modern iHD backend
   };
 
   # May help if FFmpeg/VAAPI/QSV init fails (esp. on Arc with i915):
