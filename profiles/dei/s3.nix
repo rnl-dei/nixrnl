b26aed5af4aea404b465b0ea1837bc0d22f10e77
Francisco Martins <francisco.martins@rnl.tecnico.ulisboa.pt>
2025-12-27 20:14:20 +0000

    more settings that may work?

--- profiles/dei/s3.nix
+++ profiles/dei/s3.nix
@@ -8,7 +8,11 @@
     package = pkgs.garage_2;
     settings = {
       rpc_bind_addr = "[::]:3901";
-
+      s3_api = {
+        api_bind_addr = "[::]:3900";
+        s3_region = "garage";
+        root_domain = ".s3.garage";
+      };
     };
   };
 }
