33c6b7a348c336a7900ce23e4618023b3adf9634
Francisco Martins <francisco.martins@rnl.tecnico.ulisboa.pt>
2025-12-28 09:51:25 +0000

    more s3

--- profiles/dei/s3.nix
+++ profiles/dei/s3.nix
@@ -3,10 +3,32 @@
   config,
   ...
 }:
+let
+  s3-dei-domain = "s3.blatta.${config.rnl.domain}";
+  s3-admin-dei-domain = "s3-admin.blatta.${config.rnl.domain}";
+
+in
 {
   age.secrets.garage-env-file = {
     file = ../../secrets/dei-garage-env-file.age;
     owner = "garage";
+  };
+
+  services.nginx.virtualHosts."${s3-dei-domain}" = {
+    serverName = "${s3-dei-domain}";
+    enableACME = true;
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = "http://[::1]:3900";
+    };
+  };
+  services.nginx.virtualHosts."${s3-admin-dei-domain}" = {
+    serverName = "${s3-admin-dei-domain}";
+    enableACME = true;
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = "http://[::1]:3903";
+    };
   };
   services.garage = {
     enable = true;
@@ -18,7 +40,11 @@
       s3_api = {
         api_bind_addr = "[::]:3900";
         s3_region = "garage";
-        root_domain = ".s3.garage";
+        root_domain = "${s3-dei-domain}";
+      };
+      admin = {
+        api_bind_addr = "0.0.0.0:3903";
+        metrics_require_token = true;
       };
     };
   };
