f04085ac20a36dbfa65133b5204db2ee84e648b5
Francisco Martins <francisco.martins@rnl.tecnico.ulisboa.pt>
2026-01-04 12:58:04 +0000

    should make it into a path?

--- hosts/blatta.nix
+++ hosts/blatta.nix
@@ -62,7 +62,7 @@
   };
   dei.s3.enable = true;
   dei.s3.serverName = "s3.blatta.${config.rnl.domain}";
-  dei.s3.environmentPath = config.age.secrets.garage-env-file;
+  dei.s3.environmentPath = builtins.toPath config.age.secrets.garage-env-file;
   services.nginx.virtualHosts.blatta = {
     serverName = "${config.networking.fqdn}";
     enableACME = true;
