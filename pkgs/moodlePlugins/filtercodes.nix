691619ee6c94ad7cf79d9060d2efad727baf2d04
Frost-Phoenix <hugo.pereira@rnl.tecnico.ulisboa.pt>
2026-01-16 19:43:27 +0000

    pkgs/moodlePlugins/filtercodes: update src url

--- pkgs/moodlePlugins/filtercodes.nix
+++ pkgs/moodlePlugins/filtercodes.nix
@@ -1,12 +1,12 @@
 { fetchzip, moodle-utils, ... }:
 moodle-utils.buildMoodlePlugin rec {
   name = "filtercodes";
-  version = "2024050100";
+  version = "2025102700";
 
   pluginType = "filter";
 
   src = fetchzip {
-    url = "https://moodle.org/plugins/download.php/31902/${pluginType}_${name}_moodle44_${version}.zip";
-    sha256 = "sha256-6Z+9P/ax1UKxuaoNnbEHPK3jrU5JvgjsPiQIwmF4Xac=";
+    url = "https://moodle.org/plugins/download.php/38432/${pluginType}_${name}_moodle44_${version}.zip";
+    sha256 = "sha256-cuCB2hUbb2hWEOdZ1cpvTHBu0y6MTf24bPIc+Z5MfMM=";
   };
 }
