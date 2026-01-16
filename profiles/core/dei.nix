3a48165b96a4a5994558afb6bb6cb168bfb6171d
Francisco Martins <francisco.martins@rnl.tecnico.ulisboa.pt>
2026-01-16 16:11:13 +0000

    added @109245 ssh key

--- profiles/core/dei.nix
+++ profiles/core/dei.nix
@@ -10,7 +10,7 @@
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHvLx61S/EayEzpVLvvFfwa58mIImMIU/PuAUes/t2RT leonardo.neves@virtus"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIN5+deDNLsW+PjJ6fCo4WMqyQm6xA0lbfDkJ3IWg16y0 paulo.chen@alucard"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFlyAPbpCY33eSGsbohPS824PvrEYUGf2v2ORuKBfIo6 miguel.carvalho@mlc"
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHqHh9LuuxoEpgRVcJy8BrYtvJm4oxdwUPU5BQkKO41h jose.araujo@zmws"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC2DRBpPu3BlNBr9wL9l51/SGY9xMEbaWIuRHWwYTfP+ zmws@nixos"
   ];
 
   rnl.githook.emailDestination = "robots-dei@${config.rnl.domain}";
