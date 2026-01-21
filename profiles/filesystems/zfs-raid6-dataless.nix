500ec8b36578bceb9db5bb3111684a9c54b99831
Vasco Petinga <vasco.petinga@rnl.tecnico.ulisboa.pt>
2026-01-21 16:40:07 +0000

    add host hive; add new profile for rootless zfs-raid6 (to tweak...)
    update pass

---
+++ profiles/filesystems/zfs-raid6-dataless.nix
@@ -0,0 +1,25 @@
+{ config, lib, ... }:
+{
+  boot.loader.grub = {
+    enable = lib.mkForce true; # Ensure that grub is enabled
+    devices = lib.mkForce [ ];
+
+    # Use mirrored boot even if we have only one disk
+    mirroredBoots = lib.imap0 (i: device: {
+      path = "/boot${lib.optionalString (i > 0) (toString i)}";
+      devices = [ device ];
+    }) config.rnl.storage.disks.root;
+  };
+
+  # Enable autoscrub to avoid bitrot
+  services.zfs.autoScrub = {
+    enable = true;
+    pools = [
+      "rpool"
+    ];
+
+  };
+
+  # Use ZFS with mirror on root disks and raid6 on data disks
+  rnl.storage.layout = lib.mkForce "zfs-raid6-dataless";
+}
