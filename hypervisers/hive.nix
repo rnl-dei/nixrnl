c1a471f9ba462f825b295b490902ec275accbcc9
Francisco Martins <francisco.martins@rnl.tecnico.ulisboa.pt>
2026-01-28 10:28:25 +0000

    just a bit oftesting

--- hypervisers/hive.nix
+++ hypervisers/hive.nix
@@ -1,10 +1,12 @@
-{ profiles, ... }:
+{ ... }:
 {
-  imports = with profiles; [
-    os.susetmbleweed
-    type.ceph-tumbleweed
-  ];
-
+  environment.etc = {
+    test = {
+      text = ''
+        yep. this works. fun
+      '';
+    };
+  };
   rnl.labels.location = "inf1-p01-a2";
 
 }
