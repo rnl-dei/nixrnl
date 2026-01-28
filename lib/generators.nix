0f3e8ae4f080c434dbfe15d58901b572bcfc83a6
Francisco Martins <francisco.martins@rnl.tecnico.ulisboa.pt>
2026-01-28 11:16:02 +0000

    come on

--- lib/generators.nix
+++ lib/generators.nix
@@ -126,9 +126,7 @@ let
         {
           environment.etc.test.text = hostname;
           nixpkgs.hostPlatform = "x86_64-linux";
-          config = {
-            system-manager.allowAnyDistro = true;
-          };
+          system-manager.allowAnyDistro = true;
         }
         hostPath
       ]
