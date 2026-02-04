e811483d568b369ff9838874872926dbac1c68f0
Vasco Guerreiro Vintém Morais <vasco.morais@rnl.tecnico.ulisboa.pt>
2026-02-04 16:16:58 +0000

    profiles/ns/master: correctly add zone transfers to zone

--- profiles/ns/master.nix
+++ profiles/ns/master.nix
@@ -54,7 +54,7 @@
       zones."154.136.193.in-addr.arpa" = {
         master = true;
         file = "/var/lib/dns-config/193.136.154.zone";
-        allow-transfer = [
+        slaves = [
           "193.136.128.1"
           "193.136.128.2"
           "193.136.164.1"
@@ -64,7 +64,7 @@
       zones."164.136.193.in-addr.arpa" = {
         master = true;
         file = "/var/lib/dns-config/193.136.164.zone";
-        allow-transfer = [
+        slaves = [
           "193.136.128.1"
           "193.136.128.2"
           "193.136.164.1"
