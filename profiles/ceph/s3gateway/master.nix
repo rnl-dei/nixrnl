1aa8ad3b097aafeb0efcf3f00eb55185a7128acf
Francisco Martins <francisco.martins@rnl.tecnico.ulisboa.pt>
2026-02-18 10:07:04 +0000

    more stuff

---
+++ profiles/ceph/s3gateway/master.nix
@@ -0,0 +1,48 @@
+{
+  config,
+
+  ...
+}:
+{
+  config = {
+    security.acme.acceptTerms = true;
+    services.nginx = {
+      enable = true;
+      virtualHosts = {
+        "s3.rnl.tecnico.ulisboa.pt" = {
+          forceSSL = true;
+          enableACME = true;
+          locations."/" = {
+            proxyPass = "http://localhost:80";
+          };
+        };
+      };
+    };
+    environment.etc = {
+      "etc/keepalived/keepalived.conf.test" = {
+        mode = "644";
+        text = ''
+          vrrp_instance VI_1 {
+              state MASTER
+              interface br0
+              virtual_router_id 101
+              priority 150
+              advert_int 1
+              authentication {
+                  auth_type PASS
+                  auth_pass Humiliate-Simple1-Douche-Collision
+              }
+              virtual_ipaddress {
+                  193.136.164.35/26 dev public
+              }
+              virtual_routes {
+                  default via 193.136.164.62 dev public
+              }
+          }
+
+        '';
+
+      };
+    };
+  };
+}
