5754a6d2d094da1ebe024764777ea770dcc44dbd
Vasco Guerreiro Vintém Morais <vasco.morais@rnl.tecnico.ulisboa.pt>
2026-02-25 15:27:11 +0000

    really add log policy

--- profiles/gitlab-runner/common.nix
+++ profiles/gitlab-runner/common.nix
@@ -3,5 +3,8 @@
   services.gitlab-runner = {
     enable = true;
     concurrent = 1000; # arbitrarily large number so concurrency is handled by individual limit option in each runner
+    settings = {
+      log_level = "info";
+    };
   };
 }
