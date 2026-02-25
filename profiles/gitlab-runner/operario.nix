da6a7757e4a2838b6922b148be43f2a99eb4cdd2
Vasco Guerreiro Vintém Morais <vasco.morais@rnl.tecnico.ulisboa.pt>
2026-02-25 02:43:14 +0000

    profiles/gitlab-runner/operario: add missing .env

--- profiles/gitlab-runner/operario.nix
+++ profiles/gitlab-runner/operario.nix
@@ -5,7 +5,7 @@
   services.gitlab-runner.services = {
     rnl-nix = {
       dockerImage = "ubuntu:25.10";
-      authenticationTokenConfigFile = config.age.secrets."rnl-runner".path;
+      authenticationTokenConfigFile = config.age.secrets."rnl-runner.env".path;
       registrationFlags = [ "--output-limit=16384" ];
       description = "RNL nix";
       limit = 2;
