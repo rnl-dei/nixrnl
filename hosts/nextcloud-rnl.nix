870b9e2b75cf47df9fd54df98b405e803a6b3507
Vasco Guerreiro Vintém Morais <vasco.morais@rnl.tecnico.ulisboa.pt>
2026-04-08 09:10:30 +0100

    hosts: add starter nextcloud vm config

---
+++ hosts/nextcloud-rnl.nix
@@ -0,0 +1,33 @@
+{ profiles, ... }:
+{
+  imports = with profiles; [
+    core.rnl
+    filesystems.simple-uefi
+    os.nixos
+    type.vm
+  ];
+
+  # Networking
+  networking = {
+    defaultGateway.address = "193.136.164.62";
+    defaultGateway6.address = "2001:690:2100:80::ffff:1";
+
+    interfaces.enp1s0 = {
+      ipv4.addresses = [
+        {
+          address = "193.136.164.36";
+          prefixLength = 26;
+        }
+      ];
+      ipv6.addresses = [
+        {
+          address = "2001:690:2100:81::36";
+          prefixLength = 64;
+        }
+      ];
+
+    };
+  };
+
+  rnl.labels.location = "neo";
+}
