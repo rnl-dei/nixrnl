5b8024100df42a485a89016b0eab3403846d32b4
Vasco Guerreiro Vintém Morais <vasco.morais@rnl.tecnico.ulisboa.pt>
2026-04-10 11:02:26 +0100

    correct icon

--- profiles/gitlab.nix
+++ profiles/gitlab.nix
@@ -212,9 +212,18 @@
           {
             name = "oauth2_generic";
             icon =
-              "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAEE0lEQVR4Ae2aA9ArSRCA+2zbtn3Fs23bKp"
-              + "5t23fJRs+2bdvKzuxubF7+qO9mT8HT7h/O26+qS+nUTH+rEWwQgSBXoRlDgCHAEGAIaGDs4yB4wRAP3jnBj1"
-              + "eM9OIJfV24g43yL+DqUT6cFehAhpTMYz+SxkXhL>";
+              "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAEAAAABACAYAAACqaXHeAAAEE0lEQVR4Ae2aA9ArSRCA+2zbtn3Fs23bKp5t23fJRs+2bdvKzuxubF7+qO9mT8"
+              + "HT7h/O26+qS+nUTH+rEWwQgSBXoRlDgCHAEGAIaGDs4yB4wRAP3jnBj1eM9OIJfV24g43yL+DqUT6cFehAhpTMYz+SxkXhLDJi2SJ+tyyOx/ZR+BRgWpXAf1kayeJOVvG/33"
+              + "4v+S2RK+KFQzx8CXhuRhhLeXNetOz3swd4sJRIRxEP76XwIWBnu4jJXBFLuWdioCxnd5uElbw9P8qHgMtG+LCSW8b5y3K2tRIsYjmOtUk+BDwwKYiV3DDGV5W3MJTFUl6fG+"
+              + "FDwKNTqgVcP8ZflXfuIDeui+ewgIhDpDTuYhW3FAHlUT4e4FkAByNBQ8B+3WU8rLvyXxzcg2oRwN7+7H8bjQO7yq0rYJI3g6UEMgVNAk7pr+CmWBHN8SrAEGAI2NFC8bxBbn"
+              + "VqXMl4T0b97dT+Ln4F/BtbW5xYCZsqN+YrYAgwBBgCDAGGAGeKGwGsFs2Y1il6GpvcggJYLXoegSW1EBBsBQGsFs2YxcncCGC1aBdAe3J0B/QAzVjoy9wIsJKXQDM2ehk3Ai"
+              + "zkUtBMN7oXFwJMziJYI3uCLgSygoM7YDnoRiAfaW3wx+VxJuG/GCqndQnYyuTEab5MWXy0KKbj9pc+AN1YlTPbflXYLp8OnUIga9pYwCroNBb6dtsKsEhvQKexK/tonRgd0U"
+              + "vB+yYG2QJn8wSYnEnoLe8NNcEi/bC5DbOTHKl8ERkFRCaiOQKs9BuoGQ5yFAgktzkN9xZTWMqSSLYq5/EpIazk4uHeWgrIgsN9ONQUgX6/OY0PltJYAjvoVJXzzPQqAWzLq5"
+              + "YCvoKaw0ZTFuLfVOOvzY1gKbkiskFNWc6r5TnsG1/DZ594oGtod6gLAnlsUx3Y0y6pt30pZw5wl+V0WZvEfwl3FPCY3nINrz59EOoG4lZgIRM21YkDustlx1smejJ4ZE+FHX"
+              + "RQj8Jki6hCk3m8pJbPvkUcrfaxrnRVDgXBGdpUZ9hQ9sqRPuwjpnBZJMvO+qmFk7+KHuv+A5+dHsLtBVrD4okfrP4DoSFY5Vv1dHIbs7N+ByBs5DpoKBbx25Y5CG2hn0LD6Y"
+              + "3bgJkMa37xpL/63DeFQYHdwCQubuKVnwtD3DtDU7HLh4BZXNsEAcvB4d0fWgI27BQIaVjxZnE1dCEHQUthoUeDmYgNELBK/RS3JD2kg+v7ThBnQ3f3vtDSsDlD+a5SjYKOhN"
+              + "7+XaEt6I3bg1Uy13Ruzz67bYeVvqDOzfWv6qTBQh+CtsYun6/z5bgcHNKpwAH/riV00/C8/6IOcLjDRm4Dgfg2MqylYJOuBK5R9xzp72zPrqT4nLqM5fDuAlsMDulc9XPJJl"
+              + "QO90nQJP4Ea2uIYNSGiwkAAAAASUVORK5CYII=";
             label = "Técnico ID";
             app_id = "851490151334311";
             app_secret = {
