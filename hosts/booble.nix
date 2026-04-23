7b5d6b3574087d962f15d5209337ec05686bfd47
Francisco Martins <francisco.martins@rnl.tecnico.ulisboa.pt>
2026-04-23 14:58:20 +0100

    hosts/booble: add new host

---
+++ hosts/booble.nix
@@ -0,0 +1,38 @@
+{ profiles, ... }:
+{
+  imports = with profiles; [
+    core.rnl
+    filesystems.simple-uefi
+    os.nixos
+    type.vm
+  ];
+  rnl.virtualisation.guest = {
+    description = "DEI Nextcloud and Immich";
+    createdBy = "francisco.martins";
+  };
+
+  # Networking
+  networking = {
+    defaultGateway.address = "193.136.164.62";
+    defaultGateway6.address = "2001:690:2100:80::ffff:1";
+
+    interfaces.enp1s0 = {
+      ipv4.addresses = [
+        {
+          address = "193.136.164.39";
+          prefixLength = 26;
+        }
+      ];
+      ipv6.addresses = [
+        {
+          address = "2001:690:2100:80::39";
+          prefixLength = 64;
+        }
+      ];
+
+    };
+  };
+
+  rnl.labels.location = "neo";
+
+}
