ed2981b6ef8e1723630b5c29c4a0e02ef706042b
simaolavos <simao.lavos@rnl.tecnico.ulisboa.pt>
2026-04-24 13:58:53 +0100

    Format...

--- modules/rnl/storage/nfs.nix
+++ modules/rnl/storage/nfs.nix
@@ -14,28 +14,9 @@ in
       default = 193.136 .164 .113;
     };
 
-    secretAgeFile = mkOption {
-      type = types.path;
-      description = "Path to secrets";
-    };
   };
 
   config = mkIf cfg.enable {
 
-    age.secrets.ceph_secret = {
-      file = cfg.secretAgeFile;
-      owner = "root";
-    };
-
-    fileSystems."/mnt/data/cirrus" = {
-      device = "admin@.cephfs=/";
-      fsType = "ceph";
-      options = [
-        "mon_addr=${cfg.host}:3300"
-        "name=admin"
-        "secretfile=${config.age.secrets.ceph_secret.path}"
-        "_netdev"
-      ];
-    };
   };
 }
