af3d99f810998c2d9a05d3a57146bc5373cd421e
Frost-Phoenix <hugo.pereira@rnl.tecnico.ulisboa.pt>
2026-05-07 12:02:24 +0100

    hosts/booble: add host & nextcloud config

---
+++ profiles/dei/nextcloud.nix
@@ -0,0 +1,353 @@
+{
+  config,
+  pkgs,
+  ...
+}:
+let
+  unstableTarball = builtins.fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+    sha256 = "0szij1c0cl4xvjhzb0cwvskkl54dyw11skb9hgmnhamcmmsm6bji";
+  };
+  ncPkgs =
+    import
+      (builtins.fetchTarball {
+        url = "https://github.com/NixOS/nixpkgs/archive/nixos-unstable.tar.gz";
+        sha256 = "0szij1c0cl4xvjhzb0cwvskkl54dyw11skb9hgmnhamcmmsm6bji";
+      })
+      {
+        system = pkgs.system;
+        config.allowUnfree = true;
+      };
+in
+{
+  imports = [
+    "${unstableTarball}/nixos/modules/services/networking/nextcloud-spreed-signaling.nix"
+  ];
+
+  age.secrets.dei-nextcloud-secretFile = {
+    file = ../../secrets/dei-nextcloud-secretFile.age;
+    owner = "nextcloud";
+    path = "/var/lib/nextcloud/dei-nextcloud-secretFile";
+  };
+
+  age.secrets.dei-nextcloud-admin-pass = {
+    file = ../../secrets/dei-nextcloud-admin-pass.age;
+    owner = "nextcloud";
+    path = "/var/lib/nextcloud/dei-nextcloud-admin-pass";
+  };
+
+  age.secrets.dei-nextcloud-oidc = {
+    file = ../../secrets/dei-nextcloud-oidc.age;
+    owner = "nextcloud";
+    path = "/var/lib/nextcloud/dei-nextcloud-oidc";
+  };
+
+  age.secrets.dei-spreed-backend-secret = {
+    file = ../../secrets/dei-spreed-backend-secret.age;
+    owner = "nextcloud-spreed-signaling";
+  };
+
+  age.secrets.dei-coturn-secret = {
+    file = ../../secrets/dei-coturn-secret.age;
+    owner = "turnserver";
+    group = "nextcloud-spreed-signaling";
+    mode = "0440";
+  };
+
+  age.secrets.dei-spreed-session-secret = {
+    file = ../../secrets/dei-spreed-session-secret.age;
+    owner = "nextcloud-spreed-signaling";
+  };
+
+  services.nginx.virtualHosts."${config.services.nextcloud.hostName}" = {
+    serverName = "${config.services.nextcloud.hostName}";
+    enableACME = true;
+    forceSSL = true;
+  };
+
+  services.nginx.virtualHosts."${config.virtualisation.oci-containers.containers.collabora.environment.server_name
+  }" =
+    {
+      serverName = "${config.virtualisation.oci-containers.containers.collabora.environment.server_name}";
+      enableACME = true;
+      forceSSL = true;
+      locations."/" = {
+        proxyPass = "http://[::1]:9980";
+        proxyWebsockets = true;
+      };
+    };
+
+  services.nginx.virtualHosts."${config.services.coturn.realm}" = {
+    serverName = "${config.services.coturn.realm}";
+    enableACME = true;
+    forceSSL = true;
+  };
+
+  services.nginx.virtualHosts."signaling.booble.rnl.tecnico.ulisboa.pt" = {
+    enableACME = true;
+    forceSSL = true;
+    locations."/" = {
+      proxyPass = "http://localhost:8080";
+      proxyWebsockets = true;
+    };
+  };
+
+  services.nextcloud = {
+    enable = true;
+
+    package =
+      let
+        base = ncPkgs.nextcloud33;
+      in
+      base
+      // {
+        override = args: base.override (builtins.removeAttrs args [ "caBundle" ]);
+      };
+
+    hostName = "drive.dei.tecnico.ulisboa.pt";
+
+    maxUploadSize = "16G";
+    https = true;
+
+    autoUpdateApps = {
+      enable = true;
+      startAt = "05:00:00";
+    };
+
+    extraAppsEnable = true;
+
+    extraApps = {
+      inherit (config.services.nextcloud.package.packages.apps)
+        groupfolders
+        richdocuments
+        user_oidc
+        deck
+        tasks
+        calendar
+        contacts
+        files_automatedtagging
+        spreed
+        forms
+        ;
+
+      libresign = pkgs.fetchNextcloudApp {
+        appName = "libresign";
+        appVersion = "13.1.3";
+        url = "https://github.com/LibreSign/libresign/releases/download/v13.1.3/libresign-v13.1.3.tar.gz";
+        sha256 = "sha256-rjyCZX1+vEs2TmMGQs8hI3ty4DbMNH5tBggvhklttiQ=";
+        license = "agpl3Plus";
+      };
+
+      fulltextsearch = pkgs.fetchNextcloudApp {
+        appName = "fulltextsearch";
+        appVersion = "33.0.0";
+        url = "https://github.com/nextcloud-releases/fulltextsearch/releases/download/33.0.0/fulltextsearch-33.0.0.tar.gz";
+        sha256 = "sha256-p7b+PA9z/KHB9ZRDDbO/y6CJFJLhH0xHEFc/rr1H6yg=";
+        license = "agpl3Plus";
+      };
+
+      fulltextsearch_elasticsearch = pkgs.fetchNextcloudApp {
+        appName = "fulltextsearch_elasticsearch";
+        appVersion = "33.0.0";
+        url = "https://github.com/nextcloud-releases/fulltextsearch_elasticsearch/releases/download/33.0.0/fulltextsearch_elasticsearch-33.0.0.tar.gz";
+        sha256 = "sha256-Z0A8n2XqtFkS2XpvBGrjSuGS/lkGKGoJEiR0q7UEexE=";
+        license = "agpl3Plus";
+      };
+
+      files_fulltextsearch = pkgs.fetchNextcloudApp {
+        appName = "files_fulltextsearch";
+        appVersion = "33.0.0";
+        url = "https://github.com/nextcloud-releases/files_fulltextsearch/releases/download/33.0.0/files_fulltextsearch-33.0.0.tar.gz";
+        sha256 = "sha256-5KaE6PSdDaxuEliFtr3zjnFNnRzUhsJU/8M7dvtUwEs=";
+        license = "agpl3Plus";
+      };
+
+      flow = pkgs.fetchNextcloudApp {
+        appName = "flow";
+        appVersion = "1.3.0";
+        url = "https://github.com/nextcloud-releases/flow/releases/download/v1.3.0/flow-v1.3.0.tar.gz";
+        sha256 = "sha256-2KUyBb3Y1hnIRKBpaggVk09vBs+q+DlSoJAdDs+TQ18=";
+        license = "agpl3Plus";
+      };
+
+    };
+
+    settings = {
+      overwriteProtocol = "https";
+
+      default_phone_region = "PT";
+
+      allow_local_remote_servers = true;
+
+      loglevel = 2;
+      log_type = "file";
+
+      user_oidc = {
+        auto_provision = true;
+        soft_auto_provision = true;
+        disable_account_creation = true;
+
+        login_label = "Login via Fenix";
+      };
+
+    };
+
+    database.createLocally = true;
+    configureRedis = true;
+
+    # This is just for the s3 settings atm
+    secretFile = config.age.secrets.dei-nextcloud-secretFile.path;
+
+    config = {
+      dbtype = "pgsql";
+
+      adminpassFile = config.age.secrets.dei-nextcloud-admin-pass.path;
+      adminuser = "dei-admin";
+
+      # S3 Object Storage for File Storage Backend
+      objectstore.s3 = {
+        enable = true;
+
+        useSsl = false;
+        usePathStyle = true;
+        verify_bucket_exists = false;
+
+        hostname = "193.136.164.35:7480";
+        bucket = "nextcloud-bucket";
+        secretFile = config.age.secrets.dei-nextcloud-secretFile.path; # will be replaced at runtime
+        key = "placeholder"; # will be overwritten at runtime
+      };
+    };
+  };
+
+  # NATS is used for the Spreed WebRTC signaling server
+  services.nats = {
+    enable = true;
+    port = 4222;
+  };
+
+  # Coturn configuration for WebRTC support in Spreed
+  services.coturn = {
+    enable = true;
+    no-cli = true;
+    use-auth-secret = true;
+    static-auth-secret-file = config.age.secrets.dei-coturn-secret.path;
+    realm = "turn.booble.rnl.tecnico.ulisboa.pt";
+
+    cert = "${config.security.acme.certs."turn.booble.rnl.tecnico.ulisboa.pt".directory}/fullchain.pem";
+    pkey = "${config.security.acme.certs."turn.booble.rnl.tecnico.ulisboa.pt".directory}/key.pem";
+
+    extraConfig = ''
+      listening-port=3478
+      tls-listening-port=5349
+    '';
+  };
+
+  # Spreed WebRTC signaling server for Nextcloud Talk
+  services.nextcloud-spreed-signaling = {
+    enable = true;
+    package = ncPkgs.nextcloud-spreed-signaling;
+    backends = {
+      nextcloud = {
+        urls = [ "https://${config.services.nextcloud.hostName}" ];
+        secretFile = config.age.secrets.dei-spreed-backend-secret.path;
+      };
+    };
+    settings = {
+      http = {
+        listen = "0.0.0.0:8080"; # Change this line
+      };
+      nats = {
+        url = [ "nats://127.0.0.1:4222" ];
+      };
+      turn = {
+        apikeyFile = config.age.secrets.dei-coturn-secret.path;
+        secretFile = config.age.secrets.dei-spreed-backend-secret.path;
+        servers = [
+          "turn:turn.booble.rnl.tecnico.ulisboa.pt:3478?transport=udp"
+          "turn:turn.booble.rnl.tecnico.ulisboa.pt:3478?transport=tcp"
+        ];
+      };
+      clients = {
+        internalsecretFile = config.age.secrets.dei-spreed-backend-secret.path;
+      };
+      sessions = {
+        hashkeyFile = config.age.secrets.dei-spreed-session-secret.path;
+        blockkeyFile = config.age.secrets.dei-spreed-session-secret.path;
+      };
+    };
+  };
+
+  # Allow necessary ports for Coturn and Spreed WebRTC signaling server
+  networking.firewall.allowedTCPPorts = [
+    3478
+    5349
+  ];
+  networking.firewall.allowedUDPPorts = [
+    3478
+    5349
+  ];
+  networking.firewall.allowedUDPPortRanges = [
+    {
+      from = 49152;
+      to = 65535;
+    }
+  ];
+
+  virtualisation.oci-containers.containers.collabora = {
+    image = "collabora/code:latest";
+
+    environment = {
+      aliasgroup1 = "https://${config.services.nextcloud.hostName}";
+      server_name = "collabora.booble.rnl.tecnico.ulisboa.pt";
+
+      DONT_GEN_SSL_CERT = "1";
+
+      extra_params = "--o:ssl.enable=false --o:ssl.termination=true --o:net.listen=loopback";
+    };
+
+    extraOptions = [
+      "--cap-add=MKNOD"
+      "--network=host"
+    ];
+  };
+
+  virtualisation.oci-containers.containers."elasticsearch" = {
+    image = "docker.elastic.co/elasticsearch/elasticsearch:9.3.1";
+
+    ports = [ "127.0.0.1:9200:9200" ];
+
+    environment = {
+      "discovery.type" = "single-node";
+      "xpack.security.enabled" = "false";
+      "ES_JAVA_OPTS" = "-Xms1g -Xmx8g";
+    };
+
+  };
+
+  systemd.services.nextcloud-fulltext-live = {
+    description = "Nextcloud Full Text Search Live Indexer";
+    after = [
+      "nextcloud-setup.service"
+      "podman-elasticsearch.service"
+      "postgresql.service"
+    ];
+    wantedBy = [ "multi-user.target" ];
+
+    serviceConfig = {
+      ExecStart = "${config.services.nextcloud.occ}/bin/nextcloud-occ fulltextsearch:live --quiet";
+      Restart = "always";
+      RestartSec = "10s";
+    };
+  };
+
+  programs.nix-ld.enable = true;
+  documentation.nixos.enable = false;
+
+  systemd.services.phpfpm-nextcloud.path = with pkgs; [
+    jre
+    pdftk
+    openssl
+    poppler_utils
+  ];
+}
