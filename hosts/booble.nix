4215f30558e953a4113ed2853aa5403695c9be3e
Frost-Phoenix <hugo.pereira@rnl.tecnico.ulisboa.pt>
2026-05-07 12:02:24 +0100

    hosts/booble: add host profiles/dei/nextcloud: add nextcloud config

---
+++ hosts/booble.nix
@@ -0,0 +1,53 @@
+{ config, profiles, ... }:
+{
+  imports = with profiles; [
+    core.rnl
+    core.dei
+    filesystems.simple-uefi
+    os.nixos
+    type.vm
+    webserver
+
+    dei.nextcloud
+  ];
+  rnl.virtualisation.guest = {
+    description = "DEI Nextcloud and Immich";
+    createdBy = "francisco.martins";
+  };
+
+  # Networking
+  networking = {
+    defaultGateway.address = "193.136.164.62";
+    defaultGateway6.address = "2001:690:2100:80::ffff:1";
+
+    interfaces.enp1s0 = {
+      ipv4.addresses = [
+        {
+          address = "193.136.164.39";
+          prefixLength = 26;
+        }
+      ];
+      ipv6.addresses = [
+        {
+          address = "2001:690:2100:80::39";
+          prefixLength = 64;
+        }
+      ];
+    };
+
+    hosts = {
+      "127.0.0.1" = [
+        "${config.services.nextcloud.hostName}"
+        "${config.virtualisation.oci-containers.containers.collabora.environment.server_name}"
+      ];
+      "::1" = [
+        "${config.services.nextcloud.hostName}"
+        "${config.virtualisation.oci-containers.containers.collabora.environment.server_name}"
+      ];
+    };
+
+  };
+
+  rnl.labels.location = "neo";
+
+}
