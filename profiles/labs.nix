a6eae980ff907b9f36c0668c05f9f737e0fbb3f2
Vasco Guerreiro Vintém Morais <vasco.morais@rnl.tecnico.ulisboa.pt>
2026-07-20 16:02:35 +0100

    add ';'

--- profiles/labs.nix
+++ profiles/labs.nix
@@ -105,8 +105,11 @@
     lxmls = {
       isNormalUser = true;
       description = "User for the Summer School LXMLS";
-      hashedPassword = "$y$j9T$dCKdw8Sc2SXH8zsVvhOTC0$c4XyN20Hed3a6cfHZbeuJoICEfXb/BJPhsZDIB9Dz7/"
-      extraGroups = ["volatile" "no-ssh"];
+      hashedPassword = "$y$j9T$dCKdw8Sc2SXH8zsVvhOTC0$c4XyN20Hed3a6cfHZbeuJoICEfXb/BJPhsZDIB9Dz7/";
+      extraGroups = [
+        "volatile"
+        "no-ssh"
+      ];
     };
   };
 
