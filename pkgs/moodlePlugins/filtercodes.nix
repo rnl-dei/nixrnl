4ebf510753702e86b68f27560c7b286b71b9c971
Vasco Guerreiro Vintém Morais <vasco.morais@rnl.tecnico.ulisboa.pt>
2026-09-13 12:23:50 +0100

    update topcoll; update filtercodes

--- pkgs/moodlePlugins/filtercodes.nix
+++ pkgs/moodlePlugins/filtercodes.nix
@@ -1,14 +1,14 @@
 { fetchFromGitHub, moodle-utils, ... }:
 moodle-utils.buildMoodlePlugin rec {
   name = "filtercodes";
-  version = "2025102700";
+  version = "2026050200";
 
   pluginType = "filter";
 
   src = fetchFromGitHub {
     owner = "michael-milette";
     repo = "moodle-filter_filtercodes";
-    rev = "v2.6.1";
-    sha256 = "sha256-QpaKsdNbotnDp/aWkwI/swKKOM03pF7NzZIe9Ie/YOY=";
+    rev = "v2.7.3";
+    sha256 = "sha256-uEw3z4/OJjSiPLYJnM5+l4X5XA+BeSWpre3mlTDe2YI=";
   };
 }
