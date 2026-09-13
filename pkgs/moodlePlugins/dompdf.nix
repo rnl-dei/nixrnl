53f5283695bc49285c0a25f469a457f6e07da50e
Vasco Guerreiro Vintém Morais <vasco.morais@rnl.tecnico.ulisboa.pt>
2026-09-13 12:44:43 +0100

    update dompdf, revert topcoll

--- pkgs/moodlePlugins/dompdf.nix
+++ pkgs/moodlePlugins/dompdf.nix
@@ -8,7 +8,7 @@ moodle-utils.buildMoodlePlugin rec {
   src = fetchFromGitHub {
     owner = "kiklop74";
     repo = "moodle-local_dompdf";
-    rev = "v1.1";
+    rev = "v1.5";
     sha256 = "sha256-6/nr6HU/Arzk/MIbTabwm5f+VJ4IJ3Qi8VZOJ6dpGsM=";
   };
 }
